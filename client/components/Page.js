2681e2b172940064e899bb376285631bcdb1069e
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-11-01 00:23:55 -0700

    Put meta tags into Meta component file

--- client/components/Page.js
+++ client/components/Page.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import Head from 'next/head';
 import Link from 'next/link';
+import Meta from './Meta';
 
 
 class Page extends Component {
   render() {
     return (
       <div>
-        <Head>
-          <meta name='viewport' content='width=device-width, initial-scale=1' />
-          <meta charSet='utf-8' />
-          <link rel="shortcut icon" href="/static/favicon.png" />
-          <title>Next Store</title>
-        </Head>
+        <Meta />
 
         <div>
           <div>
