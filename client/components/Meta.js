1d2a695206b85be8f37366596e1b115826c28d7d
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-11-01 22:21:02 -0700

    Move image files in static folder further into an images folder

--- client/components/Meta.js
+++ client/components/Meta.js
@@ -5,7 +5,7 @@ const Meta = () => (
   <Head>
     <meta name='viewport' content='width=device-width, initial-scale=1' />
     <meta charSet='utf-8' />
-    <link rel="shortcut icon" href="/static/favicon.png" />
+    <link rel="shortcut icon" href="/static/images/favicon.png" />
     <title>Next Store</title>
   </Head>
 )
