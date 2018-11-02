c7ff711038fc4f4bb803a1cd826d822ee0b2f0e9
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-11-01 22:28:06 -0700

    Create styled css for Nav component

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,8 +1,33 @@
 import Link from 'next/link';
+import styled from 'styled-components';
 
+
+const StyledNav = styled.ul`
+  z-index: 1;
+  margin-top: 0;
+  left: 0;
+  margin: .625rem auto 0;
+  text-align: center;
+  width: 100%;
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-pack: center;
+  justify-content: center;
+  a {
+    padding: 0 2.5rem 0.7rem 2.5rem !important;
+    -webkit-font-smoothing: antialiased;
+    outline: medium none;
+    overflow: hidden;
+    transition: all .25s ease;
+    border-bottom: 1px solid ${props => props.theme.beige};
+  }
+  a:hover {
+    border-bottom: 1px solid ${props => props.theme.orange};
+  }
+`;
 
 const Nav = () => (
-  <div>
+  <StyledNav>
     <Link href="/tops">
       <a>Tops</a>
     </Link>
@@ -24,7 +49,7 @@ const Nav = () => (
     <Link href="/wedding">
       <a>Wedding</a>
     </Link>
-  </div>
+  </StyledNav>
 );
 
 export default Nav;
