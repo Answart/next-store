4ebc6414dbe08382b720de7b83cddd9971f6655a
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-11-03 12:57:40 -0700

    Create basic query/mutation setup for user creation

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,57 @@
 const { GraphQLServer } = require('graphql-yoga')
+const bcrypt = require('bcryptjs');
+
+let dummys = [
+  {
+    id: '101010',
+    name: 'First dummy',
+    email: 'dummy@dummy.com',
+    password: 'dummydummy'
+  }
+];
 
 const typeDefs = `
 type Query {
-  info: String!
+  users: [User!]!
+  user(id: ID!): User
+}
+
+type Mutation {
+  createUser(name: String!, email: String!, password: String!): User!
+}
+
+type User {
+  id: ID!
+  name: String!
+  email: String! @unique
+  password: String!
 }
 `
 
 const resolvers = {
   Query: {
-    info: () => `This is the API of the Next Store app`
+    async users(parent, args) {
+      return dummys;
+    },
+    user(parent, args) {
+      const dummy = dummys.find(x =>  x.id === args.id );
+      return dummy;
+    }
+  },
+  Mutation: {
+    async createUser(parent, args) {
+      const id = Math.floor(Math.random() * Math.floor(999999));
+      args.email = args.email.toLowerCase();
+      const password = await bcrypt.hash(args.password, 10);
+      const user = {
+        id,
+        name: args.name,
+        email: args.email,
+        password
+      };
+      dummys.push(user);
+      return user;
+    },
   }
 }
 
