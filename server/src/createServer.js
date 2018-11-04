84f9a187c296165f757a49f8b8959759742e3780
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-11-03 17:53:55 -0700

    Move server config into new createServer file

---
+++ server/src/createServer.js
@@ -0,0 +1,41 @@
+const { GraphQLServer } = require('graphql-yoga');
+const bcrypt = require('bcryptjs');
+const db = require('./db');
+
+
+const resolvers = {
+  Query: {
+    async users(parent, args) {
+      return db.users;
+    },
+    user(parent, args) {
+      const user = db.users.find(x => x.id.toString() === args.id.toString());
+      return user;
+    }
+  },
+  Mutation: {
+    async createUser(parent, args) {
+      const id = Math.floor(Math.random() * Math.floor(999999));
+      args.email = args.email.toLowerCase();
+      const password = await bcrypt.hash(args.password, 10);
+      const user = {
+        id,
+        name: args.name,
+        email: args.email,
+        password
+      };
+      db.users.push(user);
+      return user;
+    },
+  }
+}
+
+function createServer() {
+  return new GraphQLServer({
+    typeDefs: './src/schema.graphql',
+    resolvers
+  })
+};
+
+
+module.exports = createServer;
--- server/src/index.js
+++ server/src/index.js
@@ -1,37 +1,17 @@
-const { GraphQLServer } = require('graphql-yoga')
-const bcrypt = require('bcryptjs');
-const db = require('./db');
+require('dotenv').config({ path: '.env' });
+
+const createServer = require('./createServer.js');
+const server = createServer();
 
 
-const resolvers = {
-  Query: {
-    async users(parent, args) {
-      return db.users;
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.CLIENT_URL,
     },
-    user(parent, args) {
-      const user = db.users.find(x => x.id.toString() === args.id.toString());
-      return user;
-    }
   },
-  Mutation: {
-    async createUser(parent, args) {
-      const id = Math.floor(Math.random() * Math.floor(999999));
-      args.email = args.email.toLowerCase();
-      const password = await bcrypt.hash(args.password, 10);
-      const user = {
-        id,
-        name: args.name,
-        email: args.email,
-        password
-      };
-      db.users.push(user);
-      return user;
-    },
+  details => {
+    console.log(`Server is now running on port http://localhost:${details.port}`);
   }
-}
-
-const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
-  resolvers,
-})
-server.start(() => console.log(`Server is running on http://localhost:4000`));
+);
