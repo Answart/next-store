200727c4f440a2c877036935e6d82215dd69942e
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-11-03 20:37:05 -0700

    Switch from dummy db to Prisma db

--- server/src/db.js
+++ server/src/db.js
@@ -1,12 +1,11 @@
-let db = {
-  users: [
-    {
-      id: 101010,
-      name: 'First dummy',
-      email: 'dummy@dummy.com',
-      password: 'dummydummy'
-    }
-  ]
-};
+// This file connects to the remote prisma DB and gives the ability to query it with JS
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false,
+});
 
 module.exports = db;
