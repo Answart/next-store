a2c0e91a4556c188748e539de347da79fa965e07
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-11-04 18:55:14 -0800

    Allow pages to finish Queries/Mutations before page render

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -5,6 +5,15 @@ import withApollo from '../lib/withApollo';
 
 
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
   render() {
     const { Component, pageProps, apollo } = this.props;
 
