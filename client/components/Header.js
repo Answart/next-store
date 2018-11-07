694b0309f1d3f6677d9b558ba3ff06cff9453f16
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-11-06 23:10:54 -0800

    Header authed username click reveals dropdown Links

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 import Link from 'next/link';
 import styled from 'styled-components';
 import Nav from './Nav.js';
@@ -45,59 +46,129 @@ const StyledHeader = styled.header`
   .hdr-pad {
     padding-top: 0.23rem !important;
   }
+  .hdr-dropbtn {
+    border: none;
+    color: ${props => props.theme.textGrey};
+    background-color: transparent;
+    font-size: 0.85rem;
+    font-weight: bold;
+    text-decoration: none;
+    cursor: pointer;
+  }
+  .hdr-dropbtn:hover {
+    color: ${props => props.theme.darkblue};
+  }
+  .hdr-dropdown {
+    position: relative;
+    display: inline-block;
+  }
+  .hdr-dropdown-content {
+    position: absolute;
+    display: none;
+    padding: 10px 13px;
+    background-color: white;
+    min-width: 10rem;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+  }
+  .hdr-dropdown-content a {
+    display: block;
+    padding: 6px 0;
+    color: ${props => props.theme.textGrey};
+    font-size: 0.85rem;
+    font-weight: bold;
+    text-decoration: none;
+  }
+  .hdr-dropdown-content a:hover {
+    display: block;
+    color: ${props => props.theme.textGrey};
+    text-decoration: underline ${props => props.theme.orange};
+  }
+  .show {
+    display: block;
+  }
 `;
 
-const Header = () => (
-  <StyledHeader>
-    <div className="hdr-banner">
-      <Search />
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      acctDrpdwn: false
+    };
+    this.toggAcctDrpdwn = this.toggAcctDrpdwn.bind(this);
+  }
+  toggAcctDrpdwn = e => {
+    if (!!e && e.preventDefault) e.preventDefault();
+    let currentState = this.state.acctDrpdwn;
 
-      <div className="hdr-logo">
-        <Link href="/">
-          <a>NextStore</a>
-        </Link>
-      </div>
+    this.setState({ acctDrpdwn: !currentState });
+  };
+  render() {
+    const authed = true;
+    return (
+      <StyledHeader>
+        <div className="hdr-banner">
+          <Search />
 
-      <div className="hdr-account">
-        {authed ? (
-          <>
-            <User />
-            <Link href="/sell">
-              <a>
-                <img src="/static/images/box.svg" alt="Sell" height="17" width="17" />
-              </a>
+          <div className="hdr-logo">
+            <Link href="/">
+              <a>NextStore</a>
             </Link>
-            <Link href="/account/sell">
-              <a className="hdr-pad">
-                <img src="/static/images/package.svg" alt="Sell" height="14" width="14" />
-              </a>
-            </Link>
-            <Link href="/account/cart">
-              <a className="hdr-pad">
-                <img src="/static/images/cart.svg" alt="Cart" height="13" width="13" />
-              </a>
-            </Link>
-          </>
-        ) : (
-          <>
-            <Link href="/account">
-              <a className="hdr-pad">
-                Sign In
-              </a>
-            </Link>
-            |
-            <Link href="/signup">
-              <a className="hdr-pad">
-                Join
-              </a>
-            </Link>
-          </>
-        )}
-      </div>
-    </div>
+          </div>
 
-    <Nav />
-  </StyledHeader>
-)
+          <div className="hdr-account">
+            {authed ? (
+              <>
+                <span className="hdr-dropdown">
+                  <button className="hdr-dropbtn" onClick={(e) => this.toggAcctDrpdwn(e)}>
+                    Firstname
+                  </button>
+                  <div id="myDropdown" className={this.state.acctDrpdwn ? 'hdr-dropdown-content show' : 'hdr-dropdown-content'}>
+                    <a href="/account/sell">Create Sale</a>
+                    <a href="/account/sales">Current Sales</a>
+                    <a href="/account/orders">Order History</a>
+                    <a href="/account/sold">Sale History</a>
+                    <a href="/logout">Logout</a>
+                  </div>
+                </span>
+                <Link href="/sell">
+                  <a>
+                    <img src="/static/images/box.svg" alt="Sell" height="17" width="17" />
+                  </a>
+                </Link>
+                <Link href="/account/sell">
+                  <a className="hdr-pad">
+                    <img src="/static/images/package.svg" alt="Sell" height="14" width="14" />
+                  </a>
+                </Link>
+                <Link href="/account/cart">
+                  <a className="hdr-pad">
+                    <img src="/static/images/cart.svg" alt="Cart" height="13" width="13" />
+                  </a>
+                </Link>
+              </>
+            ) : (
+              <>
+                <Link href="/account">
+                  <a className="hdr-pad">
+                    Sign In
+                  </a>
+                </Link>
+                |
+                <Link href="/signup">
+                  <a className="hdr-pad">
+                    Join
+                  </a>
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+
+        <Nav />
+      </StyledHeader>
+    );
+  }
+}
 
 export default Header;
