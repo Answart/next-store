0faa0f4531145d91f4e373d7f1c9f9e3ec1094b1
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-11-08 00:00:34 -0800

    Add ProductVariant type to server

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -16,12 +16,13 @@ const Mutation = {
   },
   async createProduct(parent, args, ctx, info) {
     // Logged in?
-    const userId = ctx.request.userId || 'cjo85can56l7p0a01z7tuoksl';
+    const userId = ctx.request.userId || 'cjo8aso9c7nzr0a01h1uyykzc';
     if (!userId) throw new Error('You must be signed in to create a product');
 
     return await ctx.db.mutation.createProduct(
       { data: {
         ...args,
+        productVariants: {},
         user: { connect: { id: userId } }
       }},
       info
@@ -30,7 +31,7 @@ const Mutation = {
   async deleteProduct(parent, args, ctx, info) {
     const id = args.id;
     // Logged in?
-    const userId = ctx.request.userId || 'cjo85can56l7p0a01z7tuoksl';
+    const userId = ctx.request.userId || 'cjo8aso9c7nzr0a01h1uyykzc';
     if (!userId) throw new Error('You must be signed in to delete a product');
     // Existing product?
     const existingProduct = await ctx.db.query.product(
@@ -40,11 +41,103 @@ const Mutation = {
     // Creator of product?
     if (existingProduct.user.id !== userId) throw new Error('You are not authorized to delete this product');
 
+    // Delete productVariations
+    const deletedVariants = await ctx.db.mutation.deleteManyProductVariants(
+      { where: {
+        product: { id }
+      }}
+    );
     // Delete product
     return await ctx.db.mutation.deleteProduct(
       { where: { id }},
       info
     );
+  },
+  async createProductVariant(parent, args, ctx, info) {
+    const productId = args.productId;
+    delete args.productId;
+    // Logged in?
+    const userId = ctx.request.userId || 'cjo8aso9c7nzr0a01h1uyykzc';
+    if (!userId) throw new Error('You must be signed in to add to a product');
+    // Existing product?
+    const existingProduct = await ctx.db.query.product(
+      { where: {
+        id: productId
+      }}
+    );
+    if (!existingProduct) throw new Error('Cannot find product with this id');
+    // Existing productVariant?
+    const [existingProductVariant] = await ctx.db.query.productVariants(
+      { where: {
+        ...args,
+        product: { id: productId }
+      }}
+    );
+
+    if (existingProductVariant) {
+      console.log('This productVariant already exists for the product. Updating quantity.');
+      return await ctx.db.mutation.updateProductVariant(
+        {
+          where: { id: existingProductVariant.id },
+          data: { quantity: existingProductVariant.quantity + args.quantity }
+        },
+        info
+      );
+    } else {
+      console.log('Creating new productVariation and updating product with new connection.');
+      const newProductVariant = await ctx.db.mutation.createProductVariant(
+        { data: {
+          ...args,
+          product: { connect: { id: productId }}
+        }},
+        info
+      );
+      const updatedProduct = await ctx.db.mutation.updateProduct(
+        {
+          where: { id: productId },
+          data: {
+            productVariants: { connect: { id: newProductVariant.id }}
+          }
+        },
+        info
+      );
+
+      return newProductVariant;
+    }
+  },
+  async removeFromProduct(parent, args, ctx, info) {
+    const id = args.id;
+    const quantity = args.quantity;
+    // Logged in?
+    const userId = ctx.request.userId || 'cjo8aso9c7nzr0a01h1uyykzc';
+    if (!userId) throw new Error('You must be signed in to remove from product');
+    // Existing productVariant?
+    const [existingProductVariant] = await ctx.db.query.productVariants(
+      { where: { id }},
+      info
+    );
+    if (!existingProductVariant) throw new Error('No productVariant found!');
+
+    const existingQuantity = existingProductVariant.quantity;
+    if (existingQuantity > quantity) {
+      // Update productVariant with decreased quantity
+      return await ctx.db.mutation.updateProductVariant(
+        {
+          where: { id },
+          data: { quantity: existingQuantity - quantity }
+        },
+        info
+      );
+    } else if (existingQuantity === quantity) {
+      console.log('blah', existingProductVariant);
+      // delete productVariant
+      return await ctx.db.mutation.deleteProductVariant(
+        { where: { id }},
+        info
+      );
+    } else {
+      throw new Error(`You cannot remove ${quantity} of ${existingQuantity} productVariants`);
+    }
   }
 };
 
--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -4,6 +4,8 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
   products: forwardTo('db'),
   product: forwardTo('db'),
+  productVariants: forwardTo('db'),
+  productVariant: forwardTo('db'),
   async users(parent, args, ctx, info) {
     return ctx.db.query.users({}, info);
   },
