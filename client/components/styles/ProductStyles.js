ed604928cd4bb336c6194b565f3441acb73c942c
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-11-10 19:39:03 -0800

    Update ProductStyles with ProductList styles

--- client/components/styles/ProductStyles.js
+++ client/components/styles/ProductStyles.js
@@ -6,7 +6,6 @@ const StyledProduct = styled.div`
   grid-auto-columns: 1fr 1fr;
   grid-gap: 3rem;
   grid-auto-flow: column;
-  min-height: 40rem;
   text-decoration: none;
   font-size: 1.1rem;
   img {
@@ -34,7 +33,7 @@ const StyledProduct = styled.div`
   }
   .buy-prdct-desc {
     border-top: 1px solid ${props => props.theme.textGrey};
-    padding: 1rem 0;
+    padding: 1rem 0 0 0;
   }
 `;
 
@@ -44,22 +43,69 @@ const StyledProductVariants = styled.div`
   .buy-prdct-slct {
     width: 280px;
   }
+  select {
+    width: 100%;
+    height: 2rem;
+    padding: 0.5rem;
+    font-size: 1rem;
+    border: 1px solid ${props => props.theme.textGrey};
+    &:focus {
+      outline: 0;
+      border-color: ${props => props.theme.darkblue};
+    }
+  }
   button {
     text-decoration: none;
     background-color: ${props => props.theme.darkblue};
     color: white;
     border: none;
-    padding: 0.5rem 1rem;
+    padding: 0.5rem 1.2rem;
   }
   .buy-prdct-price {
     padding-bottom: 2rem;
   }
-  .buy-prdct-add-btn {
+  .buy-prdct-btn {
     padding: 0.6rem 3rem;
     font-family: 'Tahoma', 'Geneva', sans-serif;
     font-weight: bold;
+    text-align: center;
+  }
+`;
+
+const StyledProductsList = styled.div`
+  display: grid;
+  grid-template-rows: 1.9rem 5rem 10fr;
+  grid-template-columns: 1fr 6fr;
+  grid-gap: 2.5rem;
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  .prod-lst-title {
+    grid-column: 1 / -1;
+    grid-row: 1 / 1;
+  }
+  .prod-lst-filters {
+    grid-column: 1 / 1;
+    grid-row: 2 / -1;
+    max-width: ${props => props.theme.maxWidth};
+  }
+  .prod-lst-pagination {
+    grid-column: 2 / -1;
+    grid-row: 2 / 2;
+    max-width: ${props => props.theme.maxWidth};
+  }
+  .prod-lst {
+    grid-column: 2 / -1;
+    grid-row: 3 / -1;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+    grid-gap: 0.5rem;
+    justify-content: space-between;
+    height: 100%;
   }
 `;
 
 export default StyledProduct;
-export { StyledProductVariants };
+export {
+  StyledProductVariants,
+  StyledProductsList
+};
