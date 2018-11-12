f7c522443198d15ebfb8e5c78382d61b952d92a5
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-11-12 13:19:47 -0800

    CreateProductVariant refers to colors/sizes in lib directory

--- client/components/CreateProductVariant.js
+++ client/components/CreateProductVariant.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react';
 import StyledForm from './styles/FormStyles';
 import StyledProduct from './styles/ProductStyles';
+import colors from '../lib/colors';
+import sizes from '../lib/sizes';
 
-
-const colors = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Purple', 'Pink', 'Brown', 'Black', 'Silver', 'White', 'Gold', 'Multi'];
-const sizes = [
-  'XXS', 'XS', 'S', 'M', 'L',
-  'XL', '1X', '2X', '3X', '4X',
-  '0', '2', '4', '6', '8',
-  '10', '12', '14', '16', '18'
-];
 
 class CreateProductVariant extends Component {
   state = {
