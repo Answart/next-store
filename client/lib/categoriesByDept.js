9e8eea2c869a63588e5afbb47be3b5d8a4e5bb1e
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-11-12 10:57:21 -0800

    Add categoriesByDept filt to lib directory

---
+++ client/lib/categoriesByDept.js
@@ -0,0 +1,25 @@
+const categoriesByDept = {
+  'Tops': [
+    'Blouses', 'Cartigans', 'Casual', 'Sweaters', 'Sweatshirts'
+  ],
+  'Bottoms': [
+    'Skirts', 'Pants', 'Jeans', 'Leggings', 'Shorts', 'Intimates'
+  ],
+  'Shoes': [
+    'Heels', 'Flats', 'Sandals', 'Wedges', 'Boots', 'Booties', 'Sneakers'
+  ],
+  'Outwear': [
+    'Jackets', 'Blazers', 'Coats', 'Cartigans'
+  ],
+  'Accessories': [
+    'Jewelry', 'Bags', 'Wallets', 'Belts', 'Sunglasses', 'Makeup', 'Hair & Hats'
+  ],
+  'Decor': [
+    'Home', 'Kitchen', 'Office', 'Lighting'
+  ],
+  'Wedding': [
+    'Dresses', 'Shoes', 'Gifts', 'Accessories'
+  ]
+};
+
+export default categoriesByDept;
