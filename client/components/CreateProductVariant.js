a3a098821a6cc8c61d9d26ff8406b35e9996bd4f
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-11-13 00:27:38 -0800

    CreateProductVariant uses ProductVariantFormFields component

--- client/components/CreateProductVariant.js
+++ client/components/CreateProductVariant.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import StyledForm from './styles/FormStyles';
 import StyledProduct from './styles/ProductStyles';
-import colors from '../lib/colors';
-import sizes from '../lib/sizes';
+import ProductVariantFormFields from './ProductVariantFormFields';
 
 
 class CreateProductVariant extends Component {
@@ -45,98 +44,22 @@ class CreateProductVariant extends Component {
           console.log('onSubmit', this.state);
         }}
       >
-
         <fieldset disabled={false} aria-busy={false}>
-          <StyledProduct>
-              <label htmlFor="size">
-                Size:
-                <select
-                  id="size"
-                  name="size"
-                  value={size}
-                  onChange={this.handleChange}
-                >
-                  <option key={0} value={''}></option>
-                  {sizes.map(sz => <option key={sz} value={sz}>{sz}</option>)}
-                </select>
-              </label>
-
-              <label htmlFor="color">
-                Color:
-                <select
-                  id="color"
-                  name="color"
-                  placeholder=""
-                  value={color}
-                  onChange={this.handleChange}
-                >
-                  <option key={0} value={''}></option>
-                  {colors.map(clr => <option key={clr} value={clr}>{clr}</option>)}
-                </select>
-              </label>
-
-              <label htmlFor="quantity">
-                Quantity:
-                <input
-                  type="number"
-                  id="quantity"
-                  name="quantity"
-                  placeholder="Quantity"
-                  min="1"
-                  value={quantity}
-                  onChange={this.handleChange}
-                  required
-                />
-              </label>
-
-              <label htmlFor="price" className="prdct-padding">
-                Price per Item:
-                <input
-                  type="number"
-                  id="price"
-                  name="price"
-                  placeholder="42.99"
-                  min="1"
-                  value={price}
-                  onChange={this.handleChange}
-                  required
-                />
-              </label>
-
-              <div>
-                <input
-                  type="checkbox"
-                  id="sale"
-                  name="sale"
-                  value={sale}
-                  onChange={this.handleChange}
-                />
-                <label htmlFor="sale" className="prdct-padding">
-                  Is this selection on sale?
-                </label>
-              </div>
-
-              {sale && (
-                <label htmlFor="salePrice" className="prdct-padding">
-                  Sale Price per Item:
-                  <input
-                    type="number"
-                    id="salePrice"
-                    name="salePrice"
-                    placeholder="22.50"
-                    min="0"
-                    disabled={!sale}
-                    value={salePrice}
-                    onChange={this.handleChange}
-                  />
-                </label>
-              )}
-          </StyledProduct>
+          <ProductVariantFormFields
+            price={price}
+            quantity={quantity}
+            color={color}
+            size={size}
+            sale={sale}
+            salePrice={salePrice}
+            handleChange={this.handleChange}
+          />
 
           <div className="form-actions prdct-padding">
-            <button type="submit" className="prdct-btn">Add Selection</button>
+            <button className="big-btn"
+              type="submit"
+            >Add Selection</button>
           </div>
-
         </fieldset>
       </StyledForm>
     );
