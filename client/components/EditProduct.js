175fa20438cf4494c7718fd68edf00f576f0ce3e
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-11-13 00:38:08 -0800

    EditProduct component calls PRODUCT_QUERY on render

--- client/components/EditProduct.js
+++ client/components/EditProduct.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
 import { StyledEditProduct } from './styles/ProductStyles';
-import product from '../lib/dummyData';
+import { Query } from 'react-apollo';
+import { PRODUCT_QUERY } from '../graphql';
 
 
 class EditProduct extends Component {
@@ -14,76 +14,89 @@ class EditProduct extends Component {
     const { activeTab } = this.state;
     const { id } = this.props;
     return (
-      <StyledEditProduct>
-        <div className="edt-prdct-title">
-          <button
-            onClick={this.setActiveTab.bind(this, 0)}
-          >{product.title}</button>
+      <Query
+        query={PRODUCT_QUERY}
+        variables={{ id }}
+      >
+        {({ data, error, loading }) => {
+          if (loading) return <p>Loading...</p>;
+          if (error) return <p>Error: {error.message}</p>;
+          const { product } = data;
+          return (
+            <StyledEditProduct>
+              <div className="edt-prdct-title">
+                <button
+                  onClick={this.setActiveTab.bind(this, 0)}
+                >{product.title}</button>
 
-          {activeTab !== 0 && (
-            <span>&#8811;
-              <button
-                onClick={this.setActiveTab.bind(this, 1)}
-              >Selections</button>
+                {activeTab !== 0 && (
+                  <span>&#8811;
+                    <button
+                      onClick={this.setActiveTab.bind(this, 1)}
+                    >Selections</button>
+
+                    {activeTab == 2 && (
+                      <span>&#8811;  Add Selection</span>
+                    )}
+                  </span>
+                )}
+              </div>
+
+              {activeTab == 0 && (
+                <div className="edt-prdct-tab">
+                  <div className="edt-prdct-navi">
+                    <button className="undrln-btn"
+                      onClick={this.setActiveTab.bind(this, 1)}
+                    >Selections &#8811;</button>
+                  </div>
+
+                  <div className="edt-prdct-cntnt">
+                    update here
+                  </div>
+                </div>
+              )}
+
+              {activeTab == 1 && (
+                <div className="edt-prdct-tab">
+                  <div className="edt-prdct-navi">
+                    <button className="undrln-btn"
+                      onClick={this.setActiveTab.bind(this, 0)}
+                    >&#8810; Product</button>
+                    <button className="undrln-btn"
+                      onClick={this.setActiveTab.bind(this, 2)}
+                    >Add Selection &#8811;</button>
+                  </div>
+
+                  <div className="edt-prdct-cntnt">
+                    {!!product.productVariants.length ? (
+                      <div>
+                        <i>{product.productVariants.length} unique Selections:</i>
+                        update variants here
+                      </div>
+                    ) : (
+                      <p>This product has no selections to remove.</p>
+                    )}
+                  </div>
+                </div>
+              )}
 
               {activeTab == 2 && (
-                <span>&#8811;  Add Selection</span>
-              )}
-            </span>
-          )}
-        </div>
+                <div className="edt-prdct-tab">
+                  <div className="edt-prdct-navi">
+                    <button className="undrln-btn"
+                      onClick={this.setActiveTab.bind(this, 1)}
+                    >&#8810; Selections</button>
+                  </div>
 
-        {activeTab == 0 && (
-          <div className="edt-prdct-tab">
-            <div className="edt-prdct-navi">
-              <button className="undrln-btn"
-                onClick={this.setActiveTab.bind(this, 1)}
-              >Selections &#8811;</button>
-            </div>
-
-            <div className="edt-prdct-cntnt">
-              Update product here
-            </div>
-          </div>
-        )}
-
-        {activeTab == 1 && (
-          <div className="edt-prdct-tab">
-            <div className="edt-prdct-navi">
-              <button className="undrln-btn"
-                onClick={this.setActiveTab.bind(this, 0)}
-              >&#8810; Product</button>
-              <button className="undrln-btn"
-                onClick={this.setActiveTab.bind(this, 2)}
-              >Add Selection &#8811;</button>
-            </div>
-
-            <div className="edt-prdct-cntnt">
-              {!!product.productVariants.length ? (
-                <div>
-                  Update existing product variant here
+                  <div className="edt-prdct-cntnt">
+                    Create product variant here
+                  </div>
                 </div>
-              ) : (
-                <p>This product has no selections to update.</p>
               )}
-            </div>
-          </div>
-        )}
-
-        {activeTab == 2 && (
-          <div className="edt-prdct-tab">
-            <div className="edt-prdct-navi">
-              <button className="undrln-btn"
-                onClick={this.setActiveTab.bind(this, 1)}
-              >&#8810; Selections</button>
-            </div>
-
-            <div className="edt-prdct-cntnt">
-              Create product variant here
-            </div>
-          </div>
-        )}
-      </StyledEditProduct>
+            </StyledEditProduct>
+          );
+        }}
+      </Query>
     )
   };
 };
