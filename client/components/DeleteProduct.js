4db478b3d487bbf933f50ebf50d6086227a6fb91
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-11-16 19:37:32 -0800

    DeleteProduct component calls DELETE_PRODUCT_MUTATION

--- client/components/DeleteProduct.js
+++ client/components/DeleteProduct.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import Router from 'next/router';
+import { DELETE_PRODUCT_MUTATION } from '../graphql';
 
 
 class DeleteProduct extends Component {
   render() {
     const { id, children } = this.props;
     return (
-      <div className="form-actions prdct-padding">
-        <button className="dlt-btn"
-          disabled={!id}
-          onClick={() => {
-            if (confirm('Are you sure you want to delete this product?')) {
-              console.log('delete product here', id)
-            }
-          }}
-        >
-          {children}
-        </button>
-      </div>
+      <Mutation
+        mutation={DELETE_PRODUCT_MUTATION}
+        variables={{ id }}
+      >
+        {(deleteProduct, { error }) => (
+          <div className="form-actions prdct-padding">
+            <button className="dlt-btn"
+              disabled={!id}
+              onClick={() => {
+                if (confirm('Are you sure you want to delete this product?')) {
+                  deleteProduct()
+                    .then((res) => {
+                      Router.push({
+                        pathname: '/shop',
+                        query: { online: true }
+                      });
+                    })
+                    .catch(err => {
+                      alert(err.message);
+                    });
+                }
+              }}
+            >
+              {children}
+            </button>
+          </div>
+        )}
+      </Mutation>
     );
   }
 }
