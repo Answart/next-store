7e521e833d996f69fc5c841eafd4b1d6db1ac3ec
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-11-18 11:17:32 -0800

    Update Signup component with form fields

--- client/components/Signup.js
+++ client/components/Signup.js
@@ -1,18 +1,68 @@
-import Link from 'next/link';
-import styled from 'styled-components';
+import React, { Component } from 'react';
+import StyledForm from './styles/FormStyles';
 
 
-const SignupStyles = styled.span`
-  text-alight: right;
-  padding: 0px 5px;
-`;
+class Signup extends Component {
+  state = {
+    name: '',
+    email: '',
+    password: ''
+  };
+  saveToState = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  render() {
+    return (
+          <StyledForm
+            method="post"
+            onSubmit={async e => {
+              e.preventDefault();
+              this.setState({ name: '', email: '', password: '' });
+            }}
+          >
+            <fieldset disabled={false} aria-busy={false}>
+              <h2>Sign Up for An Account</h2>
 
-const Signup = () => (
-  <SignupStyles>
-    <Link href="/signup">
-      <a>Join</a>
-    </Link>
-  </SignupStyles>
-);
+              <label htmlFor="email">
+                Email
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  value={this.state.email}
+                  onChange={this.saveToState}
+                />
+              </label>
+
+              <label htmlFor="name">
+                Name
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="name"
+                  value={this.state.name}
+                  onChange={this.saveToState}
+                />
+              </label>
+
+              <label htmlFor="password">
+                Password
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  value={this.state.password}
+                  onChange={this.saveToState}
+                />
+              </label>
+
+              <button className="big-btn"
+                type="submit"
+              >Sign Up!</button>
+            </fieldset>
+          </StyledForm>
+    );
+  }
+}
 
 export default Signup;
