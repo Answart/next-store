26c62c73e617d13cd7f11ac04d31167bea28e3bc
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-11-26 12:30:30 -0800

    Update lib/formatMoney

--- client/lib/formatMoney.js
+++ client/lib/formatMoney.js
@@ -4,10 +4,7 @@ export default function(amount) {
     currency: 'USD',
     minimumFractionDigits: 2,
   };
-  
-  // if its a whole, dollar amount, leave off the .00
-  if (amount % 100 === 0) options.minimumFractionDigits = 0;
   const formatter = new Intl.NumberFormat('en-US', options);
 
-  return formatter.format(amount / 100);
+  return formatter.format(amount);
 }
