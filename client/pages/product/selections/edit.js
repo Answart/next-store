9bffb742e172dae08f7cc6c81aa7b043c4e28b40
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-11-27 13:59:17 -0800

    /product/selections/edit queries SingleProduct to render EditProductVariant on page

--- client/pages/product/selections/edit.js
+++ client/pages/product/selections/edit.js
@@ -1,13 +1,48 @@
+import Link from 'next/link';
+import { StyledEditPage } from '../../../components/styles/PageStyles';
+import SingleProduct from '../../../components/SingleProduct';
 import EditProductVariant from '../../../components/EditProductVariant';
 
 
-const EditProductVariantPage = props => {
-  const { id, title } = props.query;
+const EditProductVariantsPage = props => {
+  const { id } = props.query;
   return (
-    <div>
-      <EditProductVariant id={id} productTitle={title} />
-    </div>
+    <SingleProduct variables={{ id }}>
+      {({ data, error, loading }) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) return <p>Error: {error.message}</p>;
+        const { product } = data;
+        const productTitle = product ? product.title : '';
+        return (
+          <StyledEditPage>
+            <div className="edit-pg-navi">
+              <Link href={{
+                pathname: `/product/add`,
+                query: {
+                  id,
+                  title: productTitle
+                }
+              }}><a className="undrln-btn">
+                Add Selection &#8811;
+              </a></Link>
+            </div>
+
+            <div className="edit-pg-content">
+              {!product.productVariants.length ? (
+                <p>This product does not have any selections.</p>
+              ) : (
+                <EditProductVariant
+                  productVariants={product.productVariants}
+                  productId={id}
+                />
+              )}
+            </div>
+          </StyledEditPage>
+        )
+      }}
+    </SingleProduct>
   );
 };
 
-export default EditProductVariantPage;
+
+export default EditProductVariantsPage;
