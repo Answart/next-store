7595c19c38d100fe18484cd631b7ca6d537ec234
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-11-27 13:16:09 -0800

    /product/edit queries SingleProduct to render UpdateProductForm/DeleteProduct on page

--- client/pages/product/edit.js
+++ client/pages/product/edit.js
@@ -1,12 +1,41 @@
-import EditProduct from '../../components/EditProduct';
+import Link from 'next/link';
+import { StyledEditPage } from '../../components/styles/PageStyles';
+import SingleProduct from '../../components/SingleProduct';
+import DeleteProduct from '../../components/Buttons/DeleteProduct';
+import { UpdateProductForm } from '../../components/Forms';
 
 
 const EditProductPage = props => {
+  const { id } = props.query;
   return (
-    <div>
-      <EditProduct id={props.query.id} />
-    </div>
+    <SingleProduct variables={{ id }}>
+      {({ data, error, loading }) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) return <p>Error: {error.message}</p>;
+        const { product } = data;
+        const productTitle = product ? product.title : '';
+        return (
+          <StyledEditPage>
+            <div className="edit-pg-navi">
+              <Link href={{
+                pathname: `/product/selections/edit`,
+                query: { id }
+              }}><a className="undrln-btn">
+                Selections &#8811;
+              </a></Link>
+            </div>
+
+            <div className="edit-pg-content">
+              <UpdateProductForm product={product} />
+
+              <DeleteProduct id={id}>Delete Product</DeleteProduct>
+            </div>
+          </StyledEditPage>
+        )
+      }}
+    </SingleProduct>
   );
 };
+
 
 export default EditProductPage;
