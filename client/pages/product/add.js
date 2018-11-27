6c7535d972f570d2fb3851256d06fe57ed69e8bd
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-11-27 13:28:34 -0800

    Create /product/add page for user to add productVariants to product

---
+++ client/pages/product/add.js
@@ -0,0 +1,40 @@
+import { StyledCreatePage } from '../../components/styles/PageStyles';
+import PageTitle from '../../components/PageTitle';
+import { CreateProductVariantForm } from '../../components/Forms';
+
+
+const CreateProductVariantPage = props => {
+  let { id, title } = props.query;
+  return (
+    <StyledCreatePage>
+      <PageTitle
+        page='Edit'
+        titles={[{
+            label: title,
+            href: {
+              pathname: '/product/edit',
+              query: { id }
+            }
+          }, {
+            label: 'Selections',
+            href: {
+              pathname: '/product/selections/edit',
+              query: { id }
+            }
+          }, {
+            label: 'Add Selection'
+        }]}
+      />
+
+      <div className="create-pg-form">
+        <CreateProductVariantForm
+          productTitle={title}
+          productId={id}
+        />
+      </div>
+    </StyledCreatePage>
+  );
+};
+
+
+export default CreateProductVariantPage;
