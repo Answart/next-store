97412975a3d598c1c0b03bc403893faf0fdfba6b
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-11-27 03:45:13 -0800

    Buy page shows PageTitle after SingleProduct query

--- client/pages/product/buy.js
+++ client/pages/product/buy.js
@@ -1,11 +1,40 @@
+
+import SingleProduct from '../../components/SingleProduct';
+import PageTitle from '../../components/PageTitle';
 import Product from '../../components/Product';
+import { StyledBuyPage } from '../../components/styles/PageStyles';
+import { user } from '../../lib/dummyData';
 
 
 const BuyProductPage = props => {
   return (
-    <div>
-      <Product id={props.query.id} />
-    </div>
+    <SingleProduct variables={{ id: props.query.id }}>
+      {({ data, error, loading }) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) return <p>Error: {error.message}</p>;
+        const { product } = data;
+        const productTitle = product ? product.title : '';
+        const viewerIsCreator = product
+          ? product.user.id === user.id
+          : false;
+        return (
+          <StyledBuyPage>
+            <PageTitle
+              page={product.department}
+              titles={[{ label: productTitle }]}
+            />
+
+            <div className="buy-page-content">
+              <Product
+                product={product}
+                viewerIsCreator={viewerIsCreator}
+                demoView={false}
+              />
+            </div>
+          </StyledBuyPage>
+        )
+      }}
+    </SingleProduct>
   );
 };
 
