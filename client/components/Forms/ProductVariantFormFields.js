94f8b8475d42a829265edd0546490b1f6bfdddd5
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-12-05 18:30:31 -0800

    ProductVariantFormFields uses StyledProduct

--- client/components/Forms/ProductVariantFormFields.js
+++ client/components/Forms/ProductVariantFormFields.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import StyledProduct from '../styles/ProductStyles';
 import colors from '../../lib/colors';
 import sizes from '../../lib/sizes';
 
@@ -33,94 +34,104 @@ class ProductVariantFormFields extends Component {
   render() {
     const { sale } = this.props;
     return (
-      <div>
-        <label htmlFor="size">
-          Size:
-          <select
-            id="size"
-            name="size"
-            value={this.props.size}
-            onChange={this.handleChange}
-          >
-            <option key={0} value={''}></option>
-            {sizes.map(sz => <option key={sz} value={sz}>{sz}</option>)}
-          </select>
-        </label>
-
-        <label htmlFor="color">
-          Color:
-          <select
-            id="color"
-            name="color"
-            placeholder=""
-            value={this.props.color}
-            onChange={this.handleChange}
-          >
-            <option key={0} value={''}></option>
-            {colors.map(clr => <option key={clr} value={clr}>{clr}</option>)}
-          </select>
-        </label>
-
-        <label htmlFor="quantity">
-          Quantity:
-          <input
-            type="number"
-            id="quantity"
-            name="quantity"
-            placeholder="1"
-            min="1"
-            value={this.props.quantity}
-            onChange={this.handleChange}
-            required
-          />
-        </label>
-
-        <label htmlFor="price" className="prdct-padding">
-          Price per Item:
-          <input
-            type="number"
-            id="price"
-            name="price"
-            placeholder="1.00"
-            min="1.00"
-            step="0.01"
-            value={this.props.price}
-            onChange={this.handleChange}
-            required
-          />
-        </label>
-
-        <div>
-          <input
-            type="checkbox"
-            id="sale"
-            name="sale"
-            value={sale}
-            onChange={this.handleChange}
-            checked={sale ? "checked" : ""}
-          />
-          <label htmlFor="sale" className="prdct-padding">
-            Is this selection on sale?
-          </label>
+      <StyledProduct>
+        <div className="form-imgs">
+          {!!image ? (
+            <img width="450" height="640" src={image} alt="Placeholder Image" />
+          ) : (
+            <img width="450" height="640" src="/static/images/placeholder_large.jpg" alt="Placeholder Image" />
+          )}
         </div>
 
-        {sale && (
-          <label htmlFor="salePrice" className="prdct-padding">
-            Sale Price per Item:
+        <div className="form-content">
+          <label htmlFor="size">
+            Size:
+            <select
+              id="size"
+              name="size"
+              value={this.props.size}
+              onChange={this.handleChange}
+            >
+              <option key={0} value={''}></option>
+              {sizes.map(sz => <option key={sz} value={sz}>{sz}</option>)}
+            </select>
+          </label>
+
+          <label htmlFor="color">
+            Color:
+            <select
+              id="color"
+              name="color"
+              placeholder=""
+              value={this.props.color}
+              onChange={this.handleChange}
+            >
+              <option key={0} value={''}></option>
+              {colors.map(clr => <option key={clr} value={clr}>{clr}</option>)}
+            </select>
+          </label>
+
+          <label htmlFor="quantity">
+            Quantity:
             <input
               type="number"
-              id="salePrice"
-              name="salePrice"
+              id="quantity"
+              name="quantity"
+              placeholder="1"
+              min="1"
+              value={this.props.quantity}
+              onChange={this.handleChange}
+              required
+            />
+          </label>
+
+          <label htmlFor="price" className="prdct-padding">
+            Price per Item:
+            <input
+              type="number"
+              id="price"
+              name="price"
               placeholder="1.00"
               min="1.00"
               step="0.01"
-              disabled={!sale}
-              value={this.props.salePrice}
+              value={this.props.price}
               onChange={this.handleChange}
+              required
             />
           </label>
-        )}
-      </div>
+
+          <div>
+            <input
+              type="checkbox"
+              id="sale"
+              name="sale"
+              value={sale}
+              onChange={this.handleChange}
+              checked={sale ? "checked" : ""}
+            />
+            <label htmlFor="sale" className="prdct-padding">
+              Is this selection on sale?
+            </label>
+          </div>
+
+          {sale && (
+            <label htmlFor="salePrice" className="prdct-padding">
+              Sale Price per Item:
+              <input
+                type="number"
+                id="salePrice"
+                name="salePrice"
+                placeholder="1.00"
+                min="1.00"
+                step="0.01"
+                disabled={!sale}
+                value={this.props.salePrice}
+                onChange={this.handleChange}
+              />
+            </label>
+          )}
+        </div>
+      </StyledProduct>
     );
   }
 }
