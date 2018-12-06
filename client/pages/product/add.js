3187fad2a292ddf891d384451f705b81218c0688
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-12-05 18:42:26 -0800

    /product/add page querys Product on page load

--- client/pages/product/add.js
+++ client/pages/product/add.js
@@ -1,39 +1,49 @@
 import { StyledCreatePage } from '../../components/styles/PageStyles';
+import SingleProduct from '../../components/SingleProduct';
 import PageTitle from '../../components/PageTitle';
 import { CreateProductVariantForm } from '../../components/Forms';
 
 
 const CreateProductVariantPage = props => {
-  let { id, title } = props.query;
-  const titles = [{
-      label: title,
-      href: {
-        pathname: '/product/edit',
-        query: { id }
-      }
-    }, {
-      label: 'Selections',
-      href: {
-        pathname: '/product/selections',
-        query: { id }
-      }
-    }, {
-      label: 'Add Selection'
-  }];
+  let { id } = props.query;
   return (
-    <StyledCreatePage>
-      <PageTitle
-        page='Edit'
-        titles={titles}
-      />
+    <SingleProduct variables={{ id }}>
+      {({ data, error, loading }) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) return <p>Error: {error.message}</p>;
+        const { product } = data;
+        const titles = [{
+            label: product.title,
+            href: {
+              pathname: '/product/edit',
+              query: { id }
+            }
+          }, {
+            label: 'Selections',
+            href: {
+              pathname: '/product/selections',
+              query: { id }
+            }
+          }, {
+            label: 'Add Selection'
+        }];
+        return (
+          <StyledCreatePage>
+            <PageTitle
+              page='Edit'
+              titles={titles}
+            />
 
-      <div className="create-pg-form">
-        <CreateProductVariantForm
-          productTitle={title}
-          productId={id}
-        />
-      </div>
-    </StyledCreatePage>
+            <div className="create-pg-form">
+              <CreateProductVariantForm
+                productId={id}
+                productTitle={product.title}
+              />
+            </div>
+          </StyledCreatePage>
+        )
+      }}
+    </SingleProduct>
   );
 };
 
