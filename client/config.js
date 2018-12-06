0f8615708d71e00fde0c07e5a6ad7b5b900531c9
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-12-06 01:46:12 -0800

    Update client config with cloudinary info

--- client/config.js
+++ client/config.js
@@ -1,3 +1,6 @@
 // Client side config only
 export const SERVER_URL = `http://localhost:4242`;
 export const PROD_SERVER_URL = `https://us1.prisma.sh/alexandra-6f757c/next-store-app/dev/`;
+export const CLOUDINARY_API_KEY = '376449995445936';
+export const CLOUDINARY_PRESET = 'nextstore';
+export const CLOUDINARY_SECRET = 'yk-s-3xjvyoZl8B2Ue4E01CqhZ4';
