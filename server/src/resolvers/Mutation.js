fefde831f8db3d11e7c21f5a33030906aece2b08
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-12-08 13:09:05 -0800

    Create createProductWithImage server mutation

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -184,6 +184,78 @@ const Mutation = {
       return newProductVariant;
     }
   },
+  async createProductVariantWithImage(parent, args, ctx, info) {
+    const productId = args.productId;
+    const { data, imgData } = getDataAndImgData(args);
+    delete data.id;
+    delete data.productId;
+
+    // Logged in?
+    const userId = ctx.request.userId || 'cjpd22du57cau0a22fa3o1r9r';
+    if (!userId) throw new Error('You must be signed in to add to a product');
+
+    // Existing product?
+    const existingProduct = await ctx.db.query.product({
+      where: { id: productId }
+    }, `{ id title image { id } user { id }}`);
+    if (!existingProduct) throw new Error('Cannot find product with this id');
+
+    // Existing productVariant?
+    const [existingProductVariant] = await ctx.db.query.productVariants({
+      where: {
+        ...data,
+        product: { id: productId }
+      }
+    }, `{ id quantity image { id }}`);
+
+    let imageId;
+    if (!!imgData.cloudinary_id && !!imgData.cloudinary_id.length) {
+      const newImage = await ctx.db.mutation.createImage({
+        data: {
+          ...imgData,
+          user: { connect: { id: userId } }
+        }
+      });
+      imageId = newImage.id;
+    } else {
+      // Use image from product.image
+      const productsImage = await ctx.db.query.image({
+        where: { id: existingProduct.image.id }
+      });
+      imageId = productsImage.id;
+    }
+
+    if (existingProductVariant) {
+      console.log('This productVariant already exists for the product. Updating quantity.');
+      const quantity = existingProductVariant.quantity + args.quantity;
+      return await ctx.db.mutation.updateProductVariant({
+        where: { id: existingProductVariant.id },
+        data: {
+          quantity,
+          availability: `${quantity} in Stock!`,
+          image: { connect: { id: imageId } }
+        }
+      }, info);
+    } else {
+      console.log('Creating new productVariation and updating product with new connection.');
+      const newProductVariant = await ctx.db.mutation.createProductVariant({
+        data: {
+          ...data,
+          availability: `${data.quantity} in Stock!`,
+          image: { connect: { id: imageId }},
+          product: { connect: { id: productId }}
+        }
+      }, info);
+      const updatedProduct = await ctx.db.mutation.updateProduct({
+        where: { id: productId },
+        data: {
+          productVariants: { connect: { id: newProductVariant.id }}
+        }
+      });
+
+      return newProductVariant;
+    }
+  },
   async updateProductVariant(parent, args, ctx, info) {
     const data = { ...args };
     delete data.id;
