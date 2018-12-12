e7296a8939c8aff8f0fe7fc83c28dfb6b8a8fc14
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-12-12 12:45:56 -0800

    Move ProductsListItem rendering into ProductsList component

--- client/components/ProductsList.js
+++ client/components/ProductsList.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Query } from 'react-apollo';
-import { StyledProductsList } from './styles/ProductStyles';
+import Link from 'next/link';
+import { StyledProductsList, StyledProductsListItem } from './styles/ProductStyles';
 import NotFound from './NotFound';
-import ProductsListItem from './ProductsListItem';
+import PriceTag from './PriceTag';
+import formatMoney from '../lib/formatMoney';
+import { user } from '../lib/dummyData';
 import {
   PRODUCTS_QUERY,
   ONLINE_PRODUCTS_QUERY,
@@ -42,12 +45,65 @@ class ProductsList extends Component {
           if (!products.length) return (<NotFound status={204} message='No products found.' />);
           return (
             <StyledProductsList>
-              {data.products.map(prdct =>
-                <ProductsListItem
-                  key={prdct.id}
-                  product={prdct}
-                />
-              )}
+              {products.map(prdct => {
+                const viewerIsCreator = prdct
+                  ? prdct.user.id === user.id
+                  : false;
+                const variant = prdct.productVariants.length
+                  ? prdct.productVariants[0]
+                  : null;
+                return (
+                  <StyledProductsListItem key={prdct.id} >
+                    <Link href={{
+                      pathname: "/buy",
+                      query: { id: prdct.id }
+                    }}>
+                      <a>{prdct.image && (
+                        <img width="400" height="610" src={prdct.image.image_url} alt={prdct.title} />
+                      )}</a>
+                    </Link>
+
+                    <div className="prdct-itm-info">
+                      <Link href={{
+                        pathname: "/buy",
+                        query: { id: prdct.id }
+                      }}>
+                        <a className='prdct-itm-title'>{prdct.title}</a>
+                      </Link>
+
+                      {variant && variant.price && (
+                        <div className="prdct-itm-price">
+                          <PriceTag variant={variant} />
+                        </div>
+                      )}
+
+                      {prdct.online && variant && (
+                        <div className="prdct-itm-avail">
+                          {variant.availability}
+                        </div>
+                      )}
+                    </div>
+
+                    {prdct.id && viewerIsCreator && (
+                      <span className="prdct-itm-actns">
+                        <Link href={{
+                          pathname: "/product/edit",
+                          query: { id: prdct.id }
+                        }}><a>
+                          Edit
+                        </a></Link>
+                        <button className="undrln-btn"
+                          id={prdct.id}
+                        >Delete</button>
+                      </span>
+                    )}
+
+                    {!prdct.online && (
+                      <span><i className="prdct-itm-actns">(Offline)</i></span>
+                    )}
+                  </StyledProductsListItem>
+                )
+              })};
             </StyledProductsList>
           );
         }}
