5f23d51eecfca6f5027be18fc087ea87ef4a8681
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-12-13 12:49:05 -0800

    Create UPDATE_PROD_VARIANT_MUTATION client mutation

--- client/graphql/productVariant.js
+++ client/graphql/productVariant.js
@@ -65,6 +65,32 @@ const CREATE_PROD_VARIANT_WITH_IMAGE_MUTATION = gql`
   }
 `;
 
+const UPDATE_PROD_VARIANT_MUTATION = gql`
+  mutation UPDATE_PROD_VARIANT_MUTATION(
+    $id: ID!,
+    $color: String!,
+    $size: String!,
+    $quantity: Int,
+    $price: Float,
+    $sale: Boolean,
+    $salePrice: Float,
+    $imageId: String
+  ) {
+    updateProductVariant(
+      id: $id,
+      color: $color,
+      size: $size,
+      quantity: $quantity,
+      price: $price,
+      sale: $sale,
+      salePrice: $salePrice,
+      imageId: $imageId
+    ) {
+      id
+    }
+  }
+`;
+
 const UPDATE_PROD_VARIANT_WITH_IMAGE_MUTATION = gql`
   mutation UPDATE_PROD_VARIANT_WITH_IMAGE_MUTATION(
     $id: ID!,
@@ -114,6 +140,7 @@ const DELETE_PROD_VARIANT_MUTATION = gql`
 export {
   CREATE_PROD_VARIANT_MUTATION,
   CREATE_PROD_VARIANT_WITH_IMAGE_MUTATION,
+  UPDATE_PROD_VARIANT_MUTATION,
   UPDATE_PROD_VARIANT_WITH_IMAGE_MUTATION,
   DELETE_PROD_VARIANT_MUTATION
 };
