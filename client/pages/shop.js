0d90044f0023d2381c3f202eadb9afa26005081d
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-12-14 08:15:47 -0800

    /shop page calls SHOP_PRODUCTS_QUERY on initial render

--- client/pages/shop.js
+++ client/pages/shop.js
@@ -4,50 +4,29 @@ import NotFound from '../components/NotFound';
 import PageTitle from '../components/PageTitle';
 import ProductsList from '../components/ProductsList';
 import { capWord } from '../lib/utilFns';
-import {
-  PRODUCTS_QUERY,
-  ONLINE_PRODUCTS_QUERY,
-  SELLERS_PRODUCTS_QUERY,
-  ONLINE_SELLERS_PRODUCTS_QUERY,
-  ONLINE_DEPT_PRODUCTS_QUERY
-} from '../graphql';
+import { SHOP_PRODUCTS_QUERY } from '../graphql';
 
-
-const queries = {
-  all: PRODUCTS_QUERY,
-  online: ONLINE_PRODUCTS_QUERY,
-  my: SELLERS_PRODUCTS_QUERY,
-  name: ONLINE_SELLERS_PRODUCTS_QUERY,
-  department: ONLINE_DEPT_PRODUCTS_QUERY
-};
 
 function getShopProps(variables = {}) {
   let pageLabel = '';
-  let queryType = 'all';
 
   if (variables.department) {
-    queryType = 'department';
     pageLabel = capWord(variables.department);
     variables.online = true;
   } else if (variables.name) {
     if (typeof variables.online !== 'undefined') {
-      queryType = 'name'
       pageLabel = capWord(variables.name);
     } else {
-      queryType = 'my'
-      pageLabel = 'My Products'
+      pageLabel = 'My Products';
     }
-  } else if (typeof variables.online !== 'undefined') {
-    queryType = 'online';
   }
 
-  return { variables, pageLabel, queryType };
+  return { variables, pageLabel };
 }
 
 const Shop = props => {
   const shopProps = getShopProps(props.query);
-  const { variables, pageLabel, queryType } = shopProps;
-  const query = queries[queryType];
+  const { variables, pageLabel } = shopProps;
   return (
     <StyledShopPage>
       <PageTitle
@@ -64,7 +43,7 @@ const Shop = props => {
       </div>
 
       <div className="shop-pg-lst">
-        <Query query={query}
+        <Query query={SHOP_PRODUCTS_QUERY}
           variables={variables}
         >
           {({ data, error, loading }) => {
