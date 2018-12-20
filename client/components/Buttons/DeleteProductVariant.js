95c86d9bca8fda0d644d40a71dc15b7fbb6907a3
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-12-20 01:03:58 -0800

    DeleteProductVariant component updates cache on deletion

--- client/components/Buttons/DeleteProductVariant.js
+++ client/components/Buttons/DeleteProductVariant.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import Router from 'next/router';
 import { Mutation } from 'react-apollo';
-import { DELETE_PROD_VARIANT_MUTATION } from '../../graphql';
+import { DELETE_PROD_VARIANT_MUTATION, PRODUCT_QUERY } from '../../graphql';
 
 
 class DeleteProductVariant extends Component {
   render() {
     const { productId, id, children } = this.props;
+    const update = (cache, payload) => {
+      const variables = { id: productId };
+      const data = cache.readQuery({ query: PRODUCT_QUERY, variables });
+      data.product.variants = data.product.variants.filter(variant => variant.id !== payload.data.deleteProductVariant.id);
+      cache.writeQuery({ query: PRODUCT_QUERY, variables, data });
+    };
     return (
       <Mutation mutation={DELETE_PROD_VARIANT_MUTATION}
         variables={{ id }}
+        update={update}
       >
         {(deleteProductVariant, { error }) => (
           <button className="dlt-btn"
