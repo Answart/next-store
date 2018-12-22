48c2885a5b1d84ea2d73a9ca2b0b88938b351c82
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-12-22 14:11:56 -0800

    Move lib/colors to config file

--- client/components/Forms/ProductVariantFormFields.js
+++ client/components/Forms/ProductVariantFormFields.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import StyledProduct from '../styles/ProductStyles';
-import colors from '../../lib/colors';
 import sizes from '../../lib/sizes';
+import { colors } from '../../config';
 import { uploadImageFile, destroyImageFileByToken } from '../../lib/cloudinary';
 
 
--- client/config.js
+++ client/config.js
@@ -4,3 +4,18 @@ export const PROD_SERVER_URL = `https://us1.prisma.sh/alexandra-6f757c/next-stor
 export const CLOUDINARY_API_KEY = '376449995445936';
 export const CLOUDINARY_PRESET = 'nextstore';
 export const CLOUDINARY_SECRET = 'yk-s-3xjvyoZl8B2Ue4E01CqhZ4';
+export const colors = [
+  'Red',
+  'Orange',
+  'Yellow',
+  'Green',
+  'Blue',
+  'Purple',
+  'Pink',
+  'Brown',
+  'Black',
+  'Silver',
+  'White',
+  'Gold',
+  'Multi'
+];
