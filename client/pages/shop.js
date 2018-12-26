306dce66868a675d0f366c7f6ad4160f7935c10b
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-12-26 00:58:00 -0800

    /shop page calls SHOP_PRODUCTS_QUERY and PAGINATION_QUERY on render

--- client/pages/shop.js
+++ client/pages/shop.js
@@ -5,7 +5,7 @@ import PageTitle from '../components/PageTitle';
 import ProductsList from '../components/ProductsList';
 import { capWord, getPageTitleProps } from '../lib/utilFns';
 import { user } from '../lib/dummyData';
-import { SHOP_PRODUCTS_QUERY } from '../graphql';
+import { SHOP_PRODUCTS_QUERY, PAGINATION_QUERY } from '../graphql';
 
 
 function getShopProps(pageQuery = {}) {
@@ -22,50 +22,53 @@ function getShopProps(pageQuery = {}) {
 const Shop = props => {
   const pageQuery = props.query;
   const { variables } = getShopProps(pageQuery);
+  const { pageLabel, titles } = getPageTitleProps(user, pageQuery);
   return (
-    <Query query={SHOP_PRODUCTS_QUERY}
-      variables={variables}
-    >
-      {({ data, error, loading }) => {
-        if (loading) return (<p>Loading...</p>);
-        const notFound = { message: '' };
-        const products = !!data ? data.products : [];
-        if (typeof products === 'undefined') notFound.status = 404;
-        if (!!products && !products.length) notFound.status = 204;
-        if (error) {
-          notFound.status = 400;
-          notFound.message = error.message;
-        };
-        const { pageLabel, titles } = getPageTitleProps(user, pageQuery);
-        return (
-          <StyledShopPage>
-            <PageTitle
-              page={pageLabel}
-              titles={titles}
-            />
+    <StyledShopPage>
+      <PageTitle
+        page={pageLabel}
+        titles={titles}
+      />
 
-            <div className="shop-pg-content">
-              <div>Filter here</div>
+      <Query query={SHOP_PRODUCTS_QUERY} variables={variables}>
+        {({ data: shopData, error: shopError, loading: shopLoading }) => (
+          <Query query={PAGINATION_QUERY} variables={variables}>
+            {({ data: paginData, error: paginError, loading: paginLoading }) => {
+              if (shopLoading || paginLoading) return (<p>Loading...</p>);
+              const notFound = { message: '' };
+              const products = !!shopData ? shopData.products : [];
+              const count = !!paginData ? paginData.productsConnection.aggregate.count : 0;
+              if ((!!products && !products.length) || !count) notFound.status = 204;
+              if ((typeof products === 'undefined') || (!!paginData && typeof paginData.productsConnection === 'undefined')) notFound.status = 404;
+              if (shopError || paginError) {
+                notFound.status = 400;
+                notFound.message = shopError ? shopError.message : paginError.message.replace('GraphQL error: ', '');
+              };
+              return (
+                <div className="shop-pg-content">
+                  <div>Filter here</div>
 
-              <div className="shop-pg-lst">
-                <div>Pagination here</div>
+                  <div className="shop-pg-lst">
+                    <div>Pagination here</div>
 
-                {(!!notFound.status || !products.length) ? (
-                  <NotFound status={notFound.status} message={notFound.message} />
-                ) : (
-                  <ProductsList
-                    products={products}
-                    editView={!variables.online}
-                  />
-                )}
+                    {(!!notFound.status || !products.length) ? (
+                      <NotFound status={notFound.status} message={notFound.message} />
+                    ) : (
+                      <ProductsList
+                        products={products}
+                        editView={!variables.online}
+                      />
+                    )}
 
-                <div>Pagination here</div>
-              </div>
-            </div>
-          </StyledShopPage>
-        );
-      }}
-    </Query>
+                    <div>Pagination here</div>
+                  </div>
+                </div>
+              );
+            }}
+          </Query>
+        )}
+      </Query>
+    </StyledShopPage>
   );
 };
 
