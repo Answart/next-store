ce25592293779402780ee79a50da674baca2deec
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-12-27 11:40:12 -0800

    Change ProductFormFields into a stateless component

--- client/components/Forms/ProductFormFields.js
+++ client/components/Forms/ProductFormFields.js
@@ -9,28 +9,12 @@ import { capWord } from '../../lib/utilFns';
 import { uploadImageFile, destroyImageFileByToken } from '../../lib/cloudinary';
 
 
-class ProductFormFields extends Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    department: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    online: PropTypes.bool.isRequired,
-    category: PropTypes.string,
-    brand: PropTypes.string,
-    image: PropTypes.shape({
-      id: PropTypes.string,
-      cloudinary_id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      width: PropTypes.number.isRequired,
-      height: PropTypes.number.isRequired,
-      transformation: PropTypes.string.isRequired,
-      image_url: PropTypes.string.isRequired,
-      large_image_url: PropTypes.string.isRequired,
-      delete_token: PropTypes.string
-    }),
-    saveToForm: PropTypes.func.isRequired
-  };
-  handleChange = e => {
+const ProductFormFields = props => {
+  const { department, image, online } = props;
+  let categoriesByDept = department
+    ? categories[department]
+    : categories["tops"];
+  const handleChange = e => {
     if (!!e.preventDefault) e.preventDefault();
     let { name, type, value, checked } = e.target;
     let state = {};
@@ -47,9 +31,9 @@ class ProductFormFields extends Component {
     }
     state[name] = val;
 
-    this.props.saveToForm(state);
+    props.saveToForm(state);
   };
-  handleImageChange = async e => {
+  const handleImageChange = async e => {
     const { name, type, value, files } = e.target;
     const currentImageToken = this.props.image
       ? (this.props.image.delete_token || '')
@@ -66,167 +50,182 @@ class ProductFormFields extends Component {
     }
     NProgress.done();
   }
-  render() {
-    const { department, image, online } = this.props;
-    let categoriesByDept = department
-      ? categories[department]
-      : categories["tops"];
-    return (
-      <StyledProduct>
-        <div className="form-imgs">
-          {!!image ? (
-            <img width="450" height="640" src={image.large_image_url} alt={image.name} />
-          ) : (
-            <img width="450" height="640" src="/static/images/placeholder_large.jpg" alt="Placeholder Image" />
-          )}
+  return (
+    <StyledProduct>
+      <div className="form-imgs">
+        {!!image ? (
+          <img width="450" height="640" src={image.large_image_url} alt={image.name} />
+        ) : (
+          <img width="450" height="640" src="/static/images/placeholder_large.jpg" alt="Placeholder Image" />
+        )}
+      </div>
+
+      <div className="form-content">
+        <div className="field-padding">
+          <label htmlFor="title">
+            Title:
+            <input
+              type="text"
+              id="title"
+              name="title"
+              placeholder="UGG Classic Boot"
+              value={props.title}
+              onChange={handleChange}
+              required
+            />
+          </label>
         </div>
 
-        <div className="form-content">
-          <div className="field-padding">
-            <label htmlFor="title">
-              Title:
-              <input
-                type="text"
-                id="title"
-                name="title"
-                placeholder="UGG Classic Boot"
-                value={this.props.title}
-                onChange={this.handleChange}
-                required
-              />
-            </label>
-          </div>
+        <ByCreator
+          name={user.name}
+          online={true}
+        />
 
-          <ByCreator
-            name={user.name}
-            online={true}
-          />
+        <div className="field-padding">
+          <label htmlFor="department">
+            Department:
+            <select
+              id="department"
+              name="department"
+              value={department}
+              onChange={handleChange}
+              required
+            >
+              {departments.map(dept => <option key={dept} value={dept}>{capWord(dept)}</option>)}
+            </select>
+          </label>
+        </div>
 
-          <div className="field-padding">
-            <label htmlFor="department">
-              Department:
-              <select
-                id="department"
-                name="department"
-                value={department}
-                onChange={this.handleChange}
-                required
-              >
-                {departments.map(dept => <option key={dept} value={dept}>{capWord(dept)}</option>)}
-              </select>
-            </label>
-          </div>
+        <div className="field-padding">
+          <label htmlFor="category">
+            Category:
+            <select
+              id="category"
+              name="category"
+              value={props.category}
+              onChange={handleChange}
+            >
+              <option key={0} value=''></option>
+              {categoriesByDept.map(ctgry => <option key={ctgry} value={ctgry}>{capWord(ctgry)}</option>)}
+            </select>
+          </label>
+        </div>
 
-          <div className="field-padding">
-            <label htmlFor="category">
-              Category:
-              <select
-                id="category"
-                name="category"
-                value={this.props.category}
-                onChange={this.handleChange}
-              >
-                <option key={0} value=''></option>
-                {categoriesByDept.map(ctgry => <option key={ctgry} value={ctgry}>{capWord(ctgry)}</option>)}
-              </select>
-            </label>
-          </div>
+        <div className="field-padding">
+          <label htmlFor="description">
+            Description:
+            <textarea
+              id="description"
+              name="description"
+              placeholder="UGG boots are a unisex style of sheepskin boot originating in Australia and New Zealand..."
+              value={props.description}
+              onChange={handleChange}
+              required
+            />
+          </label>
+        </div>
 
-          <div className="field-padding">
-            <label htmlFor="description">
-              Description:
-              <textarea
-                id="description"
-                name="description"
-                placeholder="UGG boots are a unisex style of sheepskin boot originating in Australia and New Zealand..."
-                value={this.props.description}
-                onChange={this.handleChange}
-                required
-              />
-            </label>
-          </div>
+        <div className="field-padding">
+          <label htmlFor="brand">
+            Brand:
+            <input
+              type="text"
+              id="brand"
+              name="brand"
+              placeholder="UGG"
+              value={props.brand}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
 
-          <div className="field-padding">
-            <label htmlFor="brand">
-              Brand:
-              <input
-                type="text"
-                id="brand"
-                name="brand"
-                placeholder="UGG"
-                value={this.props.brand}
-                onChange={this.handleChange}
-              />
-            </label>
-          </div>
-
-          <div className="field-padding">
-            <label htmlFor="status">
-              Status:
+        <div className="field-padding">
+          <label htmlFor="status">
+            Status:
+            <div className="prdct-padding">
               <div className="prdct-padding">
-                <div className="prdct-padding">
-                  <label htmlFor="offline" className="inline-lbl">
-                    <input
-                      type="radio"
-                      id="offline"
-                      name="offline"
-                      key={!online}
-                      value={!online}
-                      onChange={this.handleChange}
-                      checked={!online}
-                    />Private
-                  </label>
-                </div>
-                <div>
-                  <label htmlFor="online" className="inline-lbl">
-                    <input
-                      type="radio"
-                      id="online"
-                      name="online"
-                      key={online}
-                      value={online}
-                      onChange={this.handleChange}
-                      checked={online}
-                    />Public
-                  </label>
-                </div>
+                <label htmlFor="offline" className="inline-lbl">
+                  <input
+                    type="radio"
+                    id="offline"
+                    name="offline"
+                    key={!online}
+                    value={!online}
+                    onChange={handleChange}
+                    checked={!online}
+                  />Private
+                </label>
               </div>
-            </label>
-          </div>
-
-          <div className="field-padding">
-            <label>
-              Image:
               <div>
-                <div className="prdct-padding">
-                  <label htmlFor="image" className="lbl-button">
-                    <input
-                      type="file"
-                      id="image"
-                      name="image"
-                      onChange={this.handleImageChange}
-                    />Upload Image
-                  </label>
-                </div>
-
-                {image && !!image.name.length && (
-                  <div className="field-padding field-detail">
-                    <h5>Preview Details:</h5>
-                    <p>ID: <i>{image.cloudinary_id}</i></p>
-                    <p>Name: <i>{image.name}</i></p>
-                    <p>Dimensions: <i>{image.width}</i>W x <i>{image.height}</i>H</p>
-                    <p>Transformation: <i>{image.transformation}</i></p>
-                    <p>Url: <i>{image.image_url}</i></p>
-                  </div>
-                )}
+                <label htmlFor="online" className="inline-lbl">
+                  <input
+                    type="radio"
+                    id="online"
+                    name="online"
+                    key={online}
+                    value={online}
+                    onChange={handleChange}
+                    checked={online}
+                  />Public
+                </label>
               </div>
-            </label>
-          </div>
+            </div>
+          </label>
         </div>
-      </StyledProduct>
-    );
-  }
-}
+
+        <div className="field-padding">
+          <label>
+            Image:
+            <div>
+              <div className="prdct-padding">
+                <label htmlFor="image" className="lbl-button">
+                  <input
+                    type="file"
+                    id="image"
+                    name="image"
+                    onChange={handleImageChange}
+                  />Upload Image
+                </label>
+              </div>
+
+              {image && !!image.name.length && (
+                <div className="field-padding field-detail">
+                  <h5>Preview Details:</h5>
+                  <p>ID: <i>{image.cloudinary_id}</i></p>
+                  <p>Name: <i>{image.name}</i></p>
+                  <p>Dimensions: <i>{image.width}</i>W x <i>{image.height}</i>H</p>
+                  <p>Transformation: <i>{image.transformation}</i></p>
+                  <p>Url: <i>{image.image_url}</i></p>
+                </div>
+              )}
+            </div>
+          </label>
+        </div>
+      </div>
+    </StyledProduct>
+  );
+};
+
+ProductFormFields.propTypes = {
+  title: PropTypes.string.isRequired,
+  department: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  online: PropTypes.bool.isRequired,
+  category: PropTypes.string,
+  brand: PropTypes.string,
+  image: PropTypes.shape({
+    id: PropTypes.string,
+    cloudinary_id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired,
+    transformation: PropTypes.string.isRequired,
+    image_url: PropTypes.string.isRequired,
+    large_image_url: PropTypes.string.isRequired,
+    delete_token: PropTypes.string
+  }),
+  saveToForm: PropTypes.func.isRequired
+};
 
 
 export default ProductFormFields;
