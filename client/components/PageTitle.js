c0bc3660a919f7874de411e5668df945271ed009
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-12-27 01:34:13 -0800

    Update PageTitle component prop rendering

--- client/components/PageTitle.js
+++ client/components/PageTitle.js
@@ -4,43 +4,45 @@ import Head from 'next/head';
 import { StyledPageTitle } from './styles/PageStyles';
 
 
-const PageTitle = props => {
-  const { page, titles } = props;
-  return (
-    <StyledPageTitle>
-      <Head>
-        <title>
-          Next Store | {page}
-        </title>
-      </Head>
+const PageTitle = ({ page, titles }) => (
+  <StyledPageTitle>
+    <Head>
+      <title>
+        Next Store | {page}
+      </title>
+    </Head>
 
-      {!!page && !!page.length && (
-        <span style={{ padding: '0 1rem'}}>{page}</span>
-      )}
+    {!!page && !!page.length && (
+      <span style={{ padding: '0 1rem'}}>{page}</span>
+    )}
 
-      {!!titles && titles.map((title, i) =>
-        <span key={i}>
-          {(!!page && !!page.length) && (<>&#8811;</>)}
+    {!!titles && titles.map((title, i) =>
+      <span key={i}>
+        {(!!page && !!page.length) && (<>&#8811;</>)}
 
-          {!!title.href && (
-            <Link href={title.href}><a className="thn-btn">
-              {title.label}
-            </a></Link>
-          )}
+        {!!title.href && (
+          <Link href={title.href}><a className="thn-btn">
+            {title.label}
+          </a></Link>
+        )}
 
-          {!!title.click && (
-            <button className="thn-btn"
-              onClick={e => title.click(e)}
-            >{title.label}</button>
-          )}
+        {!!title.click && (
+          <button className="thn-btn"
+            onClick={e => title.click(e)}
+          >{title.label}</button>
+        )}
 
-          {!title.href && !title.click && (
-            <span style={{ padding: '0 1rem'}}>{title.label}</span>
-          )}
-        </span>
-      )}
-    </StyledPageTitle>
-  )
+        {!title.href && !title.click && (
+          <span style={{ padding: '0 1rem'}}>{title.label}</span>
+        )}
+      </span>
+    )}
+  </StyledPageTitle>
+);
+
+PageTitle.defaultProps = {
+  page: '',
+  titles: []
 };
 
 
