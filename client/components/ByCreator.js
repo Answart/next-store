45ec767e3913895de1087a687e5c5bcfcf033731
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-12-27 01:47:48 -0800

    Refactor ByCreator component rendering w/props

--- client/components/ByCreator.js
+++ client/components/ByCreator.js
@@ -2,19 +2,20 @@ import PropTypes from 'prop-types';
 import Link from 'next/link';
 
 
-const ByCreator = props => {
-  return (
-    <div className="prdct-creator prdct-padding">
-      By
-      <Link href={{
-        pathname: "/shop",
-        query: { name: props.name }
-      }}><a className="undrln-btn">
-        {props.name}
-      </a></Link>
-    </div>
-  )
-};
+const ByCreator = ({ name, online }) => (
+  <div className="prdct-creator prdct-padding">
+    By
+    <Link href={{
+      pathname: "/shop",
+      query: {
+        name: name,
+        online: online
+      }
+    }}><a className="undrln-btn">
+      {name}
+    </a></Link>
+  </div>
+);
 
 ByCreator.propTypes = {
   name: PropTypes.string.isRequired,
