74b66454e69737d57f6314b7437f37b040ac73f6
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-12-27 10:08:51 -0800

    Change DeleteProduct into a stateless component

--- client/components/Buttons/DeleteProduct.js
+++ client/components/Buttons/DeleteProduct.js
@@ -1,48 +1,47 @@
-import React, { Component } from 'react';
-import { Mutation } from 'react-apollo';
+import React from 'react';
 import Router from 'next/router';
+import { Mutation } from 'react-apollo';
 import { DELETE_PRODUCT_MUTATION, SHOP_PRODUCTS_QUERY } from '../../graphql';
 import { user } from '../../lib/dummyData';
 
 
-class DeleteProduct extends Component {
-  update = (cache, payload) => {
+const DeleteProduct = props => {
+  const { id, className, children } = props;
+  const update = (cache, payload) => {
     try {
       const variables = { name: user.name };
       const data = cache.readQuery({ query: SHOP_PRODUCTS_QUERY, variables });
       data.products = data.products.filter(product => product.id !== payload.data.deleteProduct.id);
       cache.writeQuery({ query: SHOP_PRODUCTS_QUERY, variables, data });
     } catch(e) {}
-  };
-  render() {
-    const { id, className, children } = this.props;
-    return (
-      <Mutation mutation={DELETE_PRODUCT_MUTATION}
-        variables={{ id }}
-        update={this.update}
-      >
-        {(deleteProduct, { error }) => (
-          <button className={className ? `${className}` : 'dlt-btn'}
-            disabled={!id}
-            onClick={() => {
-              if (confirm("Are you sure you want to delete this product?")) {
-                deleteProduct()
-                  .then((res) => {
-                    Router.push({
-                      pathname: "/shop",
-                      query: { name: user.name }
-                    });
-                  })
-                  .catch(err => alert(err.message));
-              }
-            }}
-          >
-            {children}
-          </button>
-        )}
-      </Mutation>
-    );
   }
-}
+  return (
+    <Mutation mutation={DELETE_PRODUCT_MUTATION}
+      variables={{ id }}
+      update={update}
+    >
+      {(deleteProduct, { error }) => (
+        <button className={className ? `${className}` : 'dlt-btn'}
+          disabled={!id}
+          onClick={() => {
+            if (confirm("Are you sure you want to delete this product?")) {
+              deleteProduct()
+                .catch(err => alert(err.message))
+                .then((res) => {
+                  Router.push({
+                    pathname: "/shop",
+                    query: { name: user.name }
+                  });
+                });
+            }
+          }}
+        >
+          {children}
+        </button>
+      )}
+    </Mutation>
+  )
+};
+
 
 export default DeleteProduct;
