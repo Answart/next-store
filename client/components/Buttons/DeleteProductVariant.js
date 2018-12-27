12e023209af970e4bbfabe494c61d5454bcf57d7
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-12-27 10:03:58 -0800

    Change DeleteProductVariant into a stateless component

--- client/components/Buttons/DeleteProductVariant.js
+++ client/components/Buttons/DeleteProductVariant.js
@@ -1,43 +1,47 @@
-import React, { Component } from 'react';
-import Router from 'next/router';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Mutation } from 'react-apollo';
 import { DELETE_PROD_VARIANT_MUTATION, PRODUCT_QUERY } from '../../graphql';
 
 
-class DeleteProductVariant extends Component {
-  render() {
-    const { productId, id, children } = this.props;
-    const update = (cache, payload) => {
-      const variables = { id: productId };
-      const data = cache.readQuery({ query: PRODUCT_QUERY, variables });
-      data.product.variants = data.product.variants.filter(variant => variant.id !== payload.data.deleteProductVariant.id);
-      cache.writeQuery({ query: PRODUCT_QUERY, variables, data });
-    };
-    return (
-      <Mutation mutation={DELETE_PROD_VARIANT_MUTATION}
-        variables={{ id }}
-        update={update}
-      >
-        {(deleteProductVariant, { error }) => (
-          <button className="dlt-btn"
-            disabled={!id}
-            onClick={() => {
-              if (confirm("Are you sure you want to delete this selection?")) {
-                deleteProductVariant()
-                  .then((res) => {
-                    if (this.props.postDelete) this.props.postDelete();
-                  })
-                  .catch(err => alert(err.message));
-              }
-            }}
-          >
-            {children}
-          </button>
-        )}
-      </Mutation>
-    );
-  }
-}
+const DeleteProductVariant = props => {
+  const { productId, id, children } = props;
+  const update = (cache, payload) => {
+    const variables = { id: productId };
+    const data = cache.readQuery({ query: PRODUCT_QUERY, variables });
+    data.product.variants = data.product.variants.filter(variant => variant.id !== payload.data.deleteProductVariant.id);
+    cache.writeQuery({ query: PRODUCT_QUERY, variables, data });
+  };
+  return (
+    <Mutation mutation={DELETE_PROD_VARIANT_MUTATION}
+      variables={{ id }}
+      update={update}
+    >
+      {(deleteProductVariant, { error }) => (
+        <button className="dlt-btn"
+          disabled={!id}
+          onClick={() => {
+            if (confirm("Are you sure you want to delete this selection?")) {
+              deleteProductVariant()
+                .catch(err => alert(err.message))
+                .then((res) => {
+                  if (this.props.postDelete) props.postDelete();
+                });
+            }
+          }}
+        >
+          {children}
+        </button>
+      )}
+    </Mutation>
+  );
+};
+
+DeleteProductVariant.propTypes = {
+  productId: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired
+};
 
 
 export default DeleteProductVariant;
