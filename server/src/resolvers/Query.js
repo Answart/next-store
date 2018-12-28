95cf1bd0974ffa12fa6701356d3d8e585aa0f31f
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-12-28 11:14:38 -0800

    users server query checks if logged in

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -9,6 +9,8 @@ const Query = {
     );
   },
   async users(parent, args, ctx, info) {
+    if (!ctx.request.userId) throw new Error('USERS: You must be logged in!');
+
     return ctx.db.query.users({}, info);
   },
   image: forwardTo('db'),
