623def219d91adf83b34d43804aa05015d5426ba
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-12-27 17:05:06 -0800

    Change ProductVariantFormFields into a stateless component

--- client/components/Forms/ProductVariantFormFields.js
+++ client/components/Forms/ProductVariantFormFields.js
@@ -6,29 +6,9 @@ import { colors, sizes } from '../../config';
 import { uploadImageFile, destroyImageFileByToken } from '../../lib/cloudinary';
 
 
-class ProductVariantFormFields extends Component {
-  static propTypes = {
-    price: PropTypes.number.isRequired,
-    quantity: PropTypes.number.isRequired,
-    size: PropTypes.string,
-    color: PropTypes.string,
-    sale: PropTypes.bool.isRequired,
-    salePrice: PropTypes.number.isRequired,
-    image: PropTypes.shape({
-      id: PropTypes.string,
-      cloudinary_id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      width: PropTypes.number.isRequired,
-      height: PropTypes.number.isRequired,
-      transformation: PropTypes.string.isRequired,
-      image_url: PropTypes.string.isRequired,
-      large_image_url: PropTypes.string.isRequired
-    }),
-    imageIsNew: PropTypes.bool.isRequired,
-    saveToForm: PropTypes.func.isRequired,
-    editView: PropTypes.bool.isRequired
-  };
-  handleChange = e => {
+const ProductVariantFormFields = props => {
+  const { sale, image, imageIsNew } = props;
+  const handleChange = e => {
     if (!!e.preventDefault) e.preventDefault();
     const { name, type, value, checked } = e.target;
     let val = value;
@@ -43,13 +23,13 @@ class ProductVariantFormFields extends Component {
     if (name === 'sale') state.salePrice = 1.00;
 
     state[name] = val;
-    this.props.saveToForm(state);
+    props.saveToForm(state);
   };
-  handleImageChange = async e => {
+  const handleImageChange = async e => {
     if (!!e.preventDefault) e.preventDefault();
     const { name, type, value, files } = e.target;
-    const currentImageToken = this.props.image
-      ? (this.props.image.delete_token || '')
+    const currentImageToken = props.image
+      ? (props.image.delete_token || '')
       : '';
     NProgress.start();
 
@@ -57,7 +37,7 @@ class ProductVariantFormFields extends Component {
       const imageIsNew = (value === 'true');
       if (imageIsNew && !!currentImageToken.length) await destroyImageFileByToken(currentImageToken);
 
-      this.props.saveToForm({ imageIsNew });
+      props.saveToForm({ imageIsNew });
     }
     if (type === 'file' && name === 'upload') {
       if (!files || !files.length) return;
@@ -65,183 +45,201 @@ class ProductVariantFormFields extends Component {
       if (image.error) return alert('An error occured while uploading image. Please try again later.');
       if (!!currentImageToken.length) await destroyImageFileByToken(currentImageToken);
 
-      this.props.saveToForm({ image });
+      props.saveToForm({ image });
     }
     NProgress.done();
   }
-  render() {
-    const { sale, image, imageIsNew } = this.props;
-    return (
-      <StyledProduct>
-        <div className="form-imgs">
-          {!!image ? (
-            <img width="450" height="640" src={image.large_image_url} alt={image.name} />
-          ) : (
-            <img width="450" height="640" src="/static/images/placeholder_large.jpg" alt="Placeholder Image" />
-          )}
+  return (
+    <StyledProduct>
+      <div className="form-imgs">
+        {!!image ? (
+          <img width="450" height="640" src={image.large_image_url} alt={image.name} />
+        ) : (
+          <img width="450" height="640" src="/static/images/placeholder_large.jpg" alt="Placeholder Image" />
+        )}
+      </div>
+
+      <div className="form-content">
+        <div className="field-padding">
+          <label htmlFor="size">
+            Size:
+            <select
+              id="size"
+              name="size"
+              disabled={props.editView}
+              value={props.size}
+              onChange={handleChange}
+            >
+              <option key={0} value=''></option>
+              {sizes.map(sz => <option key={sz} value={sz}>{sz}</option>)}
+            </select>
+          </label>
         </div>
 
-        <div className="form-content">
-          <div className="field-padding">
-            <label htmlFor="size">
-              Size:
-              <select
-                id="size"
-                name="size"
-                disabled={this.props.editView}
-                value={this.props.size}
-                onChange={this.handleChange}
-              >
-                <option key={0} value=''></option>
-                {sizes.map(sz => <option key={sz} value={sz}>{sz}</option>)}
-              </select>
-            </label>
-          </div>
+        <div className="field-padding">
+          <label htmlFor="color">
+            Color:
+            <select
+              id="color"
+              name="color"
+              disabled={props.editView}
+              value={props.color}
+              onChange={handleChange}
+            >
+              <option key={0} value=''></option>
+              {colors.map(clr => <option key={clr} value={clr}>{clr}</option>)}
+            </select>
+          </label>
+        </div>
 
-          <div className="field-padding">
-            <label htmlFor="color">
-              Color:
-              <select
-                id="color"
-                name="color"
-                disabled={this.props.editView}
-                value={this.props.color}
-                onChange={this.handleChange}
-              >
-                <option key={0} value=''></option>
-                {colors.map(clr => <option key={clr} value={clr}>{clr}</option>)}
-              </select>
-            </label>
-          </div>
+        <div className="field-padding">
+          <label htmlFor="quantity">
+            Quantity:
+            <input
+              type="number"
+              id="quantity"
+              name="quantity"
+              placeholder="1"
+              min="1"
+              value={props.quantity}
+              onChange={handleChange}
+              required
+            />
+          </label>
+        </div>
 
+        <div className="field-padding">
+          <label htmlFor="price">
+            Price per Item (USD):
+            <input
+              type="number"
+              id="price"
+              name="price"
+              placeholder={1.00}
+              min={1.00}
+              step={0.01}
+              value={props.price}
+              onChange={handleChange}
+              required
+            />
+          </label>
+        </div>
+
+        <div className="field-padding">
+          <label htmlFor="sale">
+            <input
+              type="checkbox"
+              id="sale"
+              name="sale"
+              key={sale}
+              value={sale}
+              onChange={handleChange}
+              checked={sale ? "checked" : ""}
+            />On Sale
+          </label>
+        </div>
+
+        {sale && (
           <div className="field-padding">
-            <label htmlFor="quantity">
-              Quantity:
+            <label htmlFor="salePrice">
+              Sale Price per Item (USD):
               <input
                 type="number"
-                id="quantity"
-                name="quantity"
-                placeholder="1"
-                min="1"
-                value={this.props.quantity}
-                onChange={this.handleChange}
-                required
-              />
-            </label>
-          </div>
-
-          <div className="field-padding">
-            <label htmlFor="price">
-              Price per Item (USD):
-              <input
-                type="number"
-                id="price"
-                name="price"
+                id="salePrice"
+                name="salePrice"
                 placeholder={1.00}
                 min={1.00}
                 step={0.01}
-                value={this.props.price}
-                onChange={this.handleChange}
-                required
+                disabled={!sale}
+                value={props.salePrice}
+                onChange={handleChange}
               />
             </label>
           </div>
+        )}
 
-          <div className="field-padding">
-            <label htmlFor="sale">
-              <input
-                type="checkbox"
-                id="sale"
-                name="sale"
-                key={sale}
-                value={sale}
-                onChange={this.handleChange}
-                checked={sale ? "checked" : ""}
-              />On Sale
-            </label>
-          </div>
-
-          {sale && (
-            <div className="field-padding">
-              <label htmlFor="salePrice">
-                Sale Price per Item (USD):
-                <input
-                  type="number"
-                  id="salePrice"
-                  name="salePrice"
-                  placeholder={1.00}
-                  min={1.00}
-                  step={0.01}
-                  disabled={!sale}
-                  value={this.props.salePrice}
-                  onChange={this.handleChange}
-                />
-              </label>
-            </div>
-          )}
-
-          <div className="field-padding">
-            <label htmlFor="image">
-              Image:
+        <div className="field-padding">
+          <label htmlFor="image">
+            Image:
+            <div className="prdct-padding">
               <div className="prdct-padding">
-                <div className="prdct-padding">
-                  <label htmlFor="productImage" className="inline-lbl">
-                    <input
-                      type="radio"
-                      id="productImage"
-                      name="productImage"
-                      key={!imageIsNew}
-                      value={false}
-                      onChange={this.handleImageChange}
-                      checked={!imageIsNew}
-                    />Same as Product Image
-                  </label>
-                </div>
-                <div>
-                  <label htmlFor="newImage" className="inline-lbl">
-                    <input
-                      type="radio"
-                      id="newImage"
-                      name="newImage"
-                      key={imageIsNew}
-                      value={true}
-                      onChange={this.handleImageChange}
-                      checked={imageIsNew}
-                    />New Image
-                  </label>
-                </div>
-
-                {imageIsNew && (
-                  <div className="field-padding">
-                    <label htmlFor="upload" className="lbl-button">
-                      <input
-                        type="file"
-                        id="upload"
-                        name="upload"
-                        onChange={this.handleImageChange}
-                      />Upload Image
-                    </label>
-                  </div>
-                )}
-
-                {!!image && !!image.name.length && (
-                  <div className="field-padding field-detail">
-                    <h5>Upload Details</h5>
-                    <p>ID: <i>{image.cloudinary_id}</i></p>
-                    <p>Name: <i>{image.name}</i></p>
-                    <p>Dimensions: <i>{image.width}</i>W x <i>{image.height}</i>H</p>
-                    <p>Transformation: <i>{image.transformation}</i></p>
-                    <p>Url: <i>{image.image_url}</i></p>
-                  </div>
-                )}
+                <label htmlFor="productImage" className="inline-lbl">
+                  <input
+                    type="radio"
+                    id="productImage"
+                    name="productImage"
+                    key={!imageIsNew}
+                    value={false}
+                    onChange={handleImageChange}
+                    checked={!imageIsNew}
+                  />Same as Product Image
+                </label>
               </div>
-            </label>
-          </div>
-        </div>
-      </StyledProduct>
-    );
-  }
-}
+              <div>
+                <label htmlFor="newImage" className="inline-lbl">
+                  <input
+                    type="radio"
+                    id="newImage"
+                    name="newImage"
+                    key={imageIsNew}
+                    value={true}
+                    onChange={handleImageChange}
+                    checked={imageIsNew}
+                  />New Image
+                </label>
+              </div>
 
+              {imageIsNew && (
+                <div className="field-padding">
+                  <label htmlFor="upload" className="lbl-button">
+                    <input
+                      type="file"
+                      id="upload"
+                      name="upload"
+                      onChange={handleImageChange}
+                    />Upload Image
+                  </label>
+                </div>
+              )}
+
+              {!!image && !!image.name.length && (
+                <div className="field-padding field-detail">
+                  <h5>Upload Details</h5>
+                  <p>ID: <i>{image.cloudinary_id}</i></p>
+                  <p>Name: <i>{image.name}</i></p>
+                  <p>Dimensions: <i>{image.width}</i>W x <i>{image.height}</i>H</p>
+                  <p>Transformation: <i>{image.transformation}</i></p>
+                  <p>Url: <i>{image.image_url}</i></p>
+                </div>
+              )}
+            </div>
+          </label>
+        </div>
+      </div>
+    </StyledProduct>
+  );
+};
+
+ProductVariantFormFields.propTypes = {
+  price: PropTypes.number.isRequired,
+  quantity: PropTypes.number.isRequired,
+  size: PropTypes.string,
+  color: PropTypes.string,
+  sale: PropTypes.bool.isRequired,
+  salePrice: PropTypes.number.isRequired,
+  image: PropTypes.shape({
+    id: PropTypes.string,
+    cloudinary_id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired,
+    transformation: PropTypes.string.isRequired,
+    image_url: PropTypes.string.isRequired,
+    large_image_url: PropTypes.string.isRequired
+  }),
+  imageIsNew: PropTypes.bool.isRequired,
+  saveToForm: PropTypes.func.isRequired,
+  editView: PropTypes.bool.isRequired
+};
 
 export default ProductVariantFormFields;
