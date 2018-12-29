9261263d3b2eec3c6824000f61e54a8dc92481b0
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-12-29 09:12:31 -0800

    Logout component calls SIGNOUT_MUTATION on click

--- client/components/Buttons/Logout.js
+++ client/components/Buttons/Logout.js
@@ -1,19 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Router from 'next/router';
+import { Mutation } from 'react-apollo';
+import { SIGNOUT_MUTATION } from '../../graphql';
 
 
-class Logout extends Component {
-  logout = e => {
-    e.preventDefault();
-  };
-  render() {
-    return (
+const Logout = () => (
+  <Mutation mutation={SIGNOUT_MUTATION}>
+    {(signout) => (
       <button className="undrln-btn"
-        onClick={this.logout}
+        onClick={() => {
+          signout()
+            .catch(err => alert(err.message))
+            .then((res) => {
+              Router.push({ pathname: '/' });
+            })
+        }}
       >
         Log Out
       </button>
-    );
-  }
-}
+    )}
+  </Mutation>
+);
+
 
 export default Logout;
