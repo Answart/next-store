e5fef45fa79defaf583b03d5f57ea8b136dd96a0
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-12-29 09:16:54 -0800

    Create me server query

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -2,6 +2,15 @@ const { forwardTo } = require('prisma-binding');
 
 
 const Query = {
+  me(parent, args, ctx, info) {
+    const id = ctx.request.userId;
+    console.log('me.id', id)
+    if (!id) return null;
+
+    return ctx.db.query.user({
+      where: { id },
+    }, info);
+  },
   async users(parent, args, ctx, info) {
     if (!ctx.request.userId) throw new Error('USERS: You must be logged in!');
 
