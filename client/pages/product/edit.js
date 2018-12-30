7297cd2714007dd58a23b28eb82e87bac487aade
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-12-30 12:51:35 -0800

    /product/edit page requires sign in

--- client/pages/product/edit.js
+++ client/pages/product/edit.js
@@ -6,45 +6,48 @@ import NotFound from '../../components/NotFound';
 import PageTitle from '../../components/PageTitle';
 import DeleteProduct from '../../components/Buttons/DeleteProduct';
 import { UpdateProductForm } from '../../components/Forms';
+import RequireSignin from '../../components/RequireSignin';
 
 
 const EditProductPage = ({ query }) => (
-  <SingleProduct variables={{ id: query.id }}>
-    {({ data, error, loading }) => {
-      if (loading) return (<p>Loading...</p>);
-      if (error) return (<NotFound status={400} message={error.message} />);
-      const { product } = data;
-      if (typeof product === 'undefined' || product === null) return (<NotFound status={404} />);
-      return (
-        <StyledEditPage>
-          <PageTitle
-            page='Edit Product'
-            titles={[{ label: product.title }]}
-          />
+  <RequireSignin>
+    <SingleProduct variables={{ id: query.id }}>
+      {({ data, error, loading }) => {
+        if (loading) return (<p>Loading...</p>);
+        if (error) return (<NotFound status={400} message={error.message} />);
+        const { product } = data;
+        if (typeof product === 'undefined' || product === null) return (<NotFound status={404} />);
+        return (
+          <StyledEditPage>
+            <PageTitle
+              page='Edit Product'
+              titles={[{ label: product.title }]}
+            />
 
-          <div className="edit-pg-navi">
-            <Link href={{
-              pathname: `/product/selections`,
-              query
-            }}><a className="undrln-btn">
-              Selections &#8811;
-            </a></Link>
-          </div>
-
-          <div className="edit-pg-content">
-            <UpdateProductForm product={product} />
-
-            <div className="edit-pg-content-footer">
-              <DeleteProduct
-                id={query.id}
-              >Delete Product</DeleteProduct>
+            <div className="edit-pg-navi">
+              <Link href={{
+                pathname: `/product/selections`,
+                query
+              }}><a className="undrln-btn">
+                Selections &#8811;
+              </a></Link>
             </div>
 
-          </div>
-        </StyledEditPage>
-      )
-    }}
-  </SingleProduct>
+            <div className="edit-pg-content">
+              <UpdateProductForm product={product} />
+
+              <div className="edit-pg-content-footer">
+                <DeleteProduct
+                  id={query.id}
+                >Delete Product</DeleteProduct>
+              </div>
+
+            </div>
+          </StyledEditPage>
+        )
+      }}
+    </SingleProduct>
+  </RequireSignin>
 );
 
 EditProductPage.propTypes = {
