20937347bf5f79cf7ab3495ca9add6a0e8fe6ccb
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-12-31 11:18:31 -0800

    Create requestPasswordReset server mutation

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,5 +1,8 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { randomBytes } = require('crypto');
+const { promisify } = require('util');
+const { transport, emailFromNextStoreSupport } = require('../mail');
 const maxAge = 1000 * 60 * 60 * 24 * 365;
 
 
@@ -49,6 +52,35 @@ const Mutation = {
     ctx.response.clearCookie('token');
 
     return { success: true, message: 'Goodbye!' };
+  },
+  async requestPasswordReset(parent, args, ctx, info) {
+    const user = await ctx.db.query.user({
+      where: { email: args.email }
+    });
+    if (!user) throw new Error(`No such user found for email ${args.email}`);
+
+    // Set a reset token and expiry on user
+    const randomBytesPromiseified = promisify(randomBytes);
+    const resetToken = (await randomBytesPromiseified(20)).toString('hex');
+    const resetTokenExpiry = Date.now() + 3600000; // 1 hour from now
+
+    await ctx.db.mutation.updateUser({
+      where: { email: args.email },
+      data: { resetToken, resetTokenExpiry },
+    });
+
+    // Email reset token
+    const mailRes = await transport.sendMail({
+      from: 'answart@sbcglobal.net',
+      to: user.email,
+      subject: 'NextStore | Password Reset Request',
+      html: emailFromNextStoreSupport(`Your Password Reset Token is here!
+      \n\n
+      <a href="${process.env
+        .CLIENT_URL}/reset?resetToken=${resetToken}">Click Here to Reset Your Password</a>`),
+    });
+
+    return { success: true, message: 'Your request has been sent! Check your email.' };
   },
   async createImage(parent, args, ctx, info) {
     const data = { ...args };
