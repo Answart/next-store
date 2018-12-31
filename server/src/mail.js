87dd48f59247522b5a603d2391e7ca17b3678625
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-12-31 11:11:38 -0800

    Create emailFromNextStoreSupport in server mail file

--- server/src/mail.js
+++ server/src/mail.js
@@ -21,5 +21,21 @@ const options = process.env.NODE_ENV === 'development'
 
 const transport = nodemailer.createTransport({ ...options });
 
+const emailFromNextStoreSupport = text => `
+  <div className="email" style="
+    border: 1px solid black;
+    padding: 20px;
+    font-family: sans-serif;
+    line-height: 2;
+    font-size: 20px;
+  ">
+    <h2>Hello There!</h2>
+    <p>${text}</p>
+
+    <p>- NextStore Support</p>
+  </div>
+`;
+
 
 exports.transport = transport;
+exports.emailFromNextStoreSupport = emailFromNextStoreSupport;
