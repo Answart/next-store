8eee751d408d1096b65d6cf10646dfecc3a38b3f
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-12-31 11:39:28 -0800

    Create resetPassword server mutation

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -82,6 +82,38 @@ const Mutation = {
 
     return { success: true, message: 'Your request has been sent! Check your email.' };
   },
+  async resetPassword(parent, args, ctx, info) {
+    // passwords match?
+    if (args.password !== args.confirmPassword) throw new Error("Your Passwords don't match!");
+
+    // token legit and not expired?
+    const [user] = await ctx.db.query.users({
+      where: {
+        resetToken: args.resetToken,
+        resetTokenExpiry_gte: Date.now() - 3600000,
+      },
+    });
+    if (!user) throw new Error('This token is either invalid or expired!');
+
+    // save new password to user and remove old resetToken fields
+    const password = await bcrypt.hash(args.password, 10);
+    const updatedUser = await ctx.db.mutation.updateUser({
+      where: { email: user.email },
+      data: {
+        password,
+        resetToken: null,
+        resetTokenExpiry: null,
+      },
+    });
+
+    const token = jwt.sign({ userId: updatedUser.id }, process.env.APP_SECRET);
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge
+    });
+
+    return updatedUser;
+  },
   async createImage(parent, args, ctx, info) {
     const data = { ...args };
     let productId;
