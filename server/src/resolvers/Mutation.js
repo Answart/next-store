fcca70dac243c2b6e2596e2166e60db809319942
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2019-01-02 09:25:34 -0800

    updateProductVariant server mutation checks if requester has permission to PRODUCTUPDATE

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -343,7 +343,13 @@ const Mutation = {
       where: { id: args.id }
     }, `{ id image { id } product { id images { id } user { id }}}`);
     if (!existingProductVariant) throw new Error(`UPDATE SELECTION: No productVariant found with id '${args.id}'.`);
-    if (existingProductVariant.product.user.id !== userId) throw new Error('UPDATE SELECTION: You are not authorized to update this selection.');
+
+    // requester has permission to update?
+    const ownsItem = existingProductVariant.product.user.id === userId;
+    const hasPermissions = ctx.request.user.permissions.some(permission =>
+      ['ADMIN', 'PRODUCTUPDATE'].includes(permission)
+    );
+    if (!ownsItem && !hasPermissions) throw new Error("UPDATE SELECTION: You are not authorized to update for this product.");
 
     // Update image?
     if (existingProductVariant.image.id !== imageId) {
