2ab802db4dbaeb6576bd1f937e6c8ba1781d80ff
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2019-01-02 08:02:22 -0800

    Create /reset page to reset password

---
+++ client/pages/reset.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import PageTitle from '../components/PageTitle';
+import { ResetPasswordForm } from '../components/Forms';
+import { StyledCreatePage } from '../components/styles/PageStyles';
+
+
+const ResetPasswordPage = ({ query }) => (
+  <StyledCreatePage>
+    <PageTitle page='Reset Your Password' />
+
+    <div className="create-pg-form">
+      <ResetPasswordForm resetToken={query.resetToken} />
+    </div>
+  </StyledCreatePage>
+);
+
+ResetPasswordPage.propTypes = {
+  query: PropTypes.shape({
+    resetToken: PropTypes.string
+  }).isRequired
+};
+
+
+export default ResetPasswordPage;
