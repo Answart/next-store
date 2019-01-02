74aceb55df6702e06f155d77a581b441e25a9f79
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2019-01-02 09:27:15 -0800

    deleteProductVariant server mutation checks if requester has permission to PRODUCTUPDATE

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -387,8 +387,17 @@ const Mutation = {
     if (!userId) throw new Error('DELETE SELECTION: You must be signed in to delete a product.');
 
     // Existing product?
-    const productVariant = await ctx.db.query.variant({ where });
-    if (!productVariant) throw new Error(`DELETE SELECTION: No selection with ID '${args.id}' found.`)
+    const [existingProductVariant] = await ctx.db.query.variants({
+      where
+    }, `{ id product { id user { id }}}`);
+    if (!existingProductVariant) throw new Error(`DELETE SELECTION: No selection with ID '${args.id}' found.`)
+
+    // requester has permission to delete?
+    const ownsItem = existingProductVariant.product.user.id === userId;
+    const hasPermissions = ctx.request.user.permissions.some(permission =>
+      ['ADMIN', 'PRODUCTUPDATE'].includes(permission)
+    );
+    if (!ownsItem && !hasPermissions) throw new Error("DELETE SELECTION: You are not authorized to delete this selection.");
 
     return await ctx.db.mutation.deleteVariant({ where }, info);
   }
