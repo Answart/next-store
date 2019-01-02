3cc48a8ea1d8f8f5ce8da324819af7fa2d2c03ec
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2019-01-02 07:58:28 -0800

    Add permissions array to config file

--- client/config.js
+++ client/config.js
@@ -4,6 +4,14 @@ export const PROD_SERVER_URL = `https://us1.prisma.sh/alexandra-6f757c/next-stor
 export const CLOUDINARY_API_KEY = '376449995445936';
 export const CLOUDINARY_PRESET = 'nextstore';
 export const CLOUDINARY_SECRET = 'yk-s-3xjvyoZl8B2Ue4E01CqhZ4';
+export const permissions = [
+  'ADMIN',
+  'USER',
+  'ITEMCREATE',
+  'ITEMUPDATE',
+  'ITEMDELETE',
+  'PERMISSIONUPDATE',
+];
 export const colors = [
   'red',
   'orange',
