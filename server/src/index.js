f22471a1332a1b150ea39cbb241cc7164ffbd57c
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2019-01-02 08:05:36 -0800

    Add middleware to set user on each request

--- server/src/index.js
+++ server/src/index.js
@@ -21,6 +21,18 @@ server.express.use((req, res, next) => {
   next();
 });
 
+// middleware to set user on each request
+server.express.use(async (req, res, next) => {
+  if (!req.userId) return next();
+
+  const user = await db.query.user({
+    where: { id: req.userId }
+  }, '{ id, permissions, email, name }');
+  req.user = user;
+
+  next();
+});
+
 server.start(
   {
     cors: {
