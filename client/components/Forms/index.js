6d973fdb7395066d11699053af72b57c0d65dc0c
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2019-01-01 19:53:55 -0800

    Add ResetPasswordForm to Forms index

--- client/components/Forms/index.js
+++ client/components/Forms/index.js
@@ -1,5 +1,6 @@
 export { SignupForm } from './SignupForm.js';
 export { SigninForm } from './SigninForm.js';
+export { ResetPasswordForm } from './ResetPasswordForm.js';
 export { CreateProductForm } from './CreateProductForm.js';
 export { CreateProductVariantForm } from './CreateProductVariantForm.js';
 export { UpdateProductForm } from './UpdateProductForm.js';
