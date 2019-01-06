1a5407ce0e6ec9c1ee4f793b4368d14c66432a79
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2019-01-05 22:36:20 -0800

    Create CheckoutCart component

---
+++ client/components/Buttons/CheckoutCart.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+
+
+const CheckoutCart = (props) => {
+  return (
+          <button className="big-btn"
+            disabled={props.disabled}
+          >
+            {props.children}
+          </button>
+  );
+};
+
+CheckoutCart.propTypes = {
+  totalQuantity: PropTypes.number.isRequired,
+  totalAmount: PropTypes.number.isRequired,
+  email: PropTypes.string.isRequired,
+  image_url: PropTypes.string,
+  disabled: PropTypes.bool.isRequired,
+  children: PropTypes.string.isRequired
+};
+
+
+export default CheckoutCart;
