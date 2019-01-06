5c7a79efd655ba31ef75c51cbc1e9c1205fc9cbc
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2019-01-05 19:02:09 -0800

    ToggleCart component calls TOGGLE_LOCAL_CARTOPEN_MUTATION on click

--- client/components/Buttons/ToggleCart.js
+++ client/components/Buttons/ToggleCart.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Mutation } from 'react-apollo';
 import { TOGGLE_LOCAL_CARTOPEN_MUTATION } from '../../graphql';
 
 
-class ToggleCart extends Component {
-  render() {
-    return (
-      <Mutation mutation={TOGGLE_LOCAL_CARTOPEN_MUTATION}>
-        {(toggleCart) => (
-          <button onClick={toggleCart}>
-            {this.props.children}
-          </button>
-        )}
-      </Mutation>
-    )
-  }
-}
+const ToggleCart = props => (
+  <Mutation mutation={TOGGLE_LOCAL_CARTOPEN_MUTATION}>
+    {(toggleCart) => (
+      <button onClick={toggleCart}>
+        {props.children}
+      </button>
+    )}
+  </Mutation>
+);
+
+ToggleCart.propTypes = {
+  children: PropTypes.object.isRequired
+};
+
 
 export default ToggleCart;
