4b082c4a4861c3b12a0ed91237da44d18cefc04e
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2019-01-05 21:39:05 -0800

    AddToCart component is now a stateless component with PropType checking

--- client/components/Buttons/AddToCart.js
+++ client/components/Buttons/AddToCart.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 
-class AddToCart extends Component {
-  addToCart = e => {
-    e.preventDefault();
-  };
-  render() {
-    const { variant, disabled } = this.props;
-    console.log('this.PROPS', this.props);
-    console.log('THIS', this);
-    return (
+const AddToCart = ({ variant, disabled }) => (
       <button className="big-btn"
         disabled={disabled}
-        onClick={this.addToCart}
+        onClick={(e) => e.preventDefault;console.log('adding', variant.id)}
       >
         Add To Cart
       </button>
-    );
-  }
-}
+);
+
+AddToCart.propTypes = {
+  variant: PropTypes.shape({
+    id: PropTypes.string.isRequired
+  }).isRequired,
+  disabled: PropTypes.bool.isRequired
+};
+
 
 export default AddToCart;
