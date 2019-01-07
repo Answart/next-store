440dbc35b3dccb77150345765a34cc367f21554d
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2019-01-07 11:20:06 -0800

    Add capWord util to CartItem

--- client/components/CartItem.js
+++ client/components/CartItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 import { StyledCartItem } from './styles/TableStyles';
+import { capWord } from '../lib/utils';
 
 
 const CartItem = ({ id, quantity, variant }) => {
