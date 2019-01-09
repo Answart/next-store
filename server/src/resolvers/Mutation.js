9e2fdeb139f656fac907068e51aff2d531063f4a
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2019-01-09 15:40:48 -0800

    Create removeFromCart server mutation

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -449,6 +449,21 @@ const Mutation = {
         variant: { connect: { id: args.id } }
       }
     }, info);
+  },
+  async removeFromCart(parent, args, ctx, info) {
+    const where = { id: args.id };
+
+    // cartItem exists?
+    const cartItem = await ctx.db.query.cartItem({
+      where
+    }, `{ id user { id }}`);
+
+    if (!cartItem) throw new Error('No CartItem Found!');
+    if (cartItem.user.id !== ctx.request.userId) throw new Error('REMOVE FROM CART: Cannot remove item from cart that is not yours.');
+
+    return ctx.db.mutation.deleteCartItem({
+      where
+    }, info);
   }
 };
 
