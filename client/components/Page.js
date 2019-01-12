aacc5b3c8e76b837a2908f484886f8e6e0d20fd2
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2019-01-11 19:54:06 -0800

    StyledPage and injectGlobal get theme colors from props.theme

--- client/components/Page.js
+++ client/components/Page.js
@@ -29,15 +29,16 @@ const theme = {
 
 const StyledPage = styled.div`
   background: white;
-  color: ${theme.black};
+  color: ${props => props.theme.darkGrey};
   a.undrln-btn, button.undrln-btn {
     padding: 0.5rem;
     font-size: 1rem;
     font-weight: bold;
     -webkit-font-smoothing: antialiased;
-    &:hover {
-      color: ${theme.darkBlue};
-      text-decoration: underline ${theme.coral};
+    color: ${props => props.theme.darkGrey};
+    &:hover, &:active, &:focus {
+      color: ${props => props.theme.darkBlue};
+      text-decoration: underline ${props => props.theme.coral};
     }
   }
   a.big-btn, button.big-btn {
@@ -46,19 +47,19 @@ const StyledPage = styled.div`
     color: white;
     font-size: 1.3rem;
     font-weight: bold;
-    background-color: ${theme.darkBlue};
-    &:hover {
+    background-color: ${props => props.theme.darkBlue};
+    &:hover, &:active, &:focus {
       color: white;
-      background-color: ${theme.black};
+      background-color: ${props => props.theme.black};
     }
   }
   a.dlt-btn, button.dlt-btn  {
     padding: 0.5rem 1.4rem;
-    color: ${theme.red};
+    color: ${props => props.theme.red};
     font-size: 1.1rem;
     font-weight: normal;
-    &:hover {
-      color: ${theme.red};
+    &:hover, &:active, &:focus {
+      color: ${props => props.theme.red};
       font-weight: bold;
     }
   }
@@ -70,12 +71,12 @@ const StyledPage = styled.div`
     -webkit-font-smoothing: antialiased;
     font-weight: normal;
     &:hover, &:active, &:focus {
-      color: ${theme.darkBlue};
+      color: ${props => props.theme.darkBlue};
       font-weight: normal;
     }
   }
   .line-through {
-    text-decoration: line-through ${theme.red};
+    text-decoration: line-through ${props => props.theme.red};
   }
   .inner-page {
     max-width: ${props => props.theme.maxWidth};
@@ -106,6 +107,7 @@ injectGlobal`
   html {
     box-sizing: border-box;
     font-size: 10px;
+    color: ${props => props.theme.darkGrey};
   }
   *, *:before, *:after {
     box-sizing: inherit;
@@ -113,27 +115,27 @@ injectGlobal`
   body {
     padding: 0;
     margin: 0;
-    font-size: 1.2rem;
+    font-size: 1rem;
     letter-spacing: 0.02rem;
     font-family: 'Tahoma', 'Geneva', sans-serif;
-    color: ${theme.darkGrey};
+    color: ${props => props.theme.darkGrey};
     -webkit-font-smoothing: antialiased;
   }
   div {
     display: block;
   }
   h1, h2, h3, h4, h5 {
-    color: ${theme.darkBlue};
+    color: ${props => props.theme.darkBlue};
     padding: 0;
     margin: 0;
   }
   a {
     font-size: 0.85rem;
     font-weight: bold;
-    color: ${theme.darkGrey};
+    color: ${props => props.theme.darkGrey};
     text-decoration: none;
     &:hover, &:active, &:focus {
-      color: ${theme.darkBlue};
+      color: ${props => props.theme.darkBlue};
       outline: 0;
     }
   }
@@ -141,12 +143,12 @@ injectGlobal`
     font-family: 'Tahoma', 'Geneva', sans-serif;
     text-decoration: none;
     font-weight: normal;
-    color: ${theme.darkGrey};
+    color: ${props => props.theme.darkGrey};
     cursor: pointer;
     border: 0;
     text-decoration: none;
     background-color: inherit;
-    &[disabled] {
+    &:disabled {
       opacity: 0.5;
     }
     &:hover, &:active, &:focus {
@@ -158,111 +160,111 @@ injectGlobal`
     width: 2rem;
     height: 2rem;
     border-radius: 10px;
-    background: ${theme.red};
-    background-color: ${theme.red};
+    background: ${props => props.theme.red};
+    background-color: ${props => props.theme.red};
   }
   .orange-sample {
     width: 2rem;
     height: 2rem;
     border-radius: 10px;
-    background: ${theme.orange};
-    background-color: ${theme.orange};
+    background: ${props => props.theme.orange};
+    background-color: ${props => props.theme.orange};
   }
   .yellow-sample {
     width: 2rem;
     height: 2rem;
     border-radius: 10px;
-    background: ${theme.yellow};
-    background-color: ${theme.yellow};
+    background: ${props => props.theme.yellow};
+    background-color: ${props => props.theme.yellow};
   }
   .green-sample {
     width: 2rem;
     height: 2rem;
     border-radius: 10px;
-    background: ${theme.green};
-    background-color: ${theme.green};
+    background: ${props => props.theme.green};
+    background-color: ${props => props.theme.green};
   }
   .blue-sample {
     width: 2rem;
     height: 2rem;
     border-radius: 10px;
-    background: ${theme.blue};
-    background-color: ${theme.blue};
+    background: ${props => props.theme.blue};
+    background-color: ${props => props.theme.blue};
   }
   .purple-sample {
     width: 2rem;
     height: 2rem;
     border-radius: 10px;
-    background: ${theme.purple};
-    background-color: ${theme.purple};
+    background: ${props => props.theme.purple};
+    background-color: ${props => props.theme.purple};
   }
   .pink-sample {
     width: 2rem;
     height: 2rem;
     border-radius: 10px;
-    background: ${theme.pink};
-    background-color: ${theme.pink};
+    background: ${props => props.theme.pink};
+    background-color: ${props => props.theme.pink};
   }
   .brown-sample {
     width: 2rem;
     height: 2rem;
     border-radius: 10px;
-    background: ${theme.brown};
-    background-color: ${theme.brown};
+    background: ${props => props.theme.brown};
+    background-color: ${props => props.theme.brown};
   }
   .black-sample {
     width: 2rem;
     height: 2rem;
     border-radius: 10px;
-    background: ${theme.black};
-    background-color: ${theme.black};
+    background: ${props => props.theme.black};
+    background-color: ${props => props.theme.black};
   }
   .white-sample {
     width: 2rem;
     height: 2rem;
     border-radius: 10px;
     background: white;
-    border: 1px solid ${theme.lightGrey};
+    border: 1px solid ${props => props.theme.lightGrey};
   }
   .silver-sample {
     width: 2rem;
     height: 2rem;
     border-radius: 10px;
-    background: ${theme.lightGrey}; /* Old browsers */
-    background: -o-linear-gradient(45deg, ${theme.lightGrey} 20%, white 85%, white 100%); /* For Opera 11.1 to 12.0 */
-    background: -moz-linear-gradient(45deg, ${theme.lightGrey} 20%, white 85%, white 100%); /* FF3.6-15 */
-    background: -webkit-linear-gradient(45deg, ${theme.lightGrey} 20%, white 85%, white 100%); /* Chrome10-25,Safari5.1-6 */
-    background: linear-gradient(45deg, ${theme.lightGrey} 20%, white 85%, white 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='${theme.lightGrey}', endColorstr='white',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+    background: ${props => props.theme.lightGrey}; /* Old browsers */
+    background: -o-linear-gradient(45deg, ${props => props.theme.lightGrey} 20%, white 85%, white 100%); /* For Opera 11.1 to 12.0 */
+    background: -moz-linear-gradient(45deg, ${props => props.theme.lightGrey} 20%, white 85%, white 100%); /* FF3.6-15 */
+    background: -webkit-linear-gradient(45deg, ${props => props.theme.lightGrey} 20%, white 85%, white 100%); /* Chrome10-25,Safari5.1-6 */
+    background: linear-gradient(45deg, ${props => props.theme.lightGrey} 20%, white 85%, white 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='${props => props.theme.lightGrey}', endColorstr='white',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
   }
   .gold-sample {
     width: 2rem;
     height: 2rem;
     border-radius: 10px;
     background: gold; /* Old browsers */
-    background: -moz-linear-gradient(45deg, ${theme.gold} 20%, white 85%, white 100%); /* FF3.6-15 */
-    background: -webkit-linear-gradient(45deg, ${theme.gold} 20%, white 85%, white 100%); /* Chrome10-25,Safari5.1-6 */
-    background: linear-gradient(45deg, ${theme.gold} 20%, white 85%, white 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='${theme.gold}', endColorstr='white',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+    background: -moz-linear-gradient(45deg, ${props => props.theme.gold} 20%, white 85%, white 100%); /* FF3.6-15 */
+    background: -webkit-linear-gradient(45deg, ${props => props.theme.gold} 20%, white 85%, white 100%); /* Chrome10-25,Safari5.1-6 */
+    background: linear-gradient(45deg, ${props => props.theme.gold} 20%, white 85%, white 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='${props => props.theme.gold}', endColorstr='white',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
   }
   .multi-sample {
     width: 2rem;
     height: 2rem;
     border-radius: 10px;
-    background: ${theme.beige}; /* Old browsers */
+    background: ${props => props.theme.beige}; /* Old browsers */
     background:
-      -moz-linear-gradient(circle at 50% 0, ${theme.red}, rgba(255,0,0,0) 50.71%),
-      -moz-linear-gradient(circle at 6.7% 75%, ${theme.blue}, rgba(0,0,255,0) 70.71%),
-      -moz-linear-gradient(circle at 93.3% 75%, ${theme.yellow}, rgba(0,255,0,0) 100%) beige; /* FF3.6-15 ??? */
+      -moz-linear-gradient(circle at 50% 0, ${props => props.theme.red}, rgba(255,0,0,0) 50.71%),
+      -moz-linear-gradient(circle at 6.7% 75%, ${props => props.theme.blue}, rgba(0,0,255,0) 70.71%),
+      -moz-linear-gradient(circle at 93.3% 75%, ${props => props.theme.yellow}, rgba(0,255,0,0) 100%) beige; /* FF3.6-15 ??? */
     background:
-      -webkit-linear-gradient(circle at 50% 0, ${theme.red}, rgba(255,0,0,0) 50.71%),
-      -webkit-linear-gradient(circle at 6.7% 75%, ${theme.blue}, rgba(0,0,255,0) 70.71%),
-      -webkit-linear-gradient(circle at 93.3% 75%, ${theme.yellow}, rgba(0,255,0,0) 100%) beige; /* Chrome10-25,Safari5.1-6 */
+      -webkit-linear-gradient(circle at 50% 0, ${props => props.theme.red}, rgba(255,0,0,0) 50.71%),
+      -webkit-linear-gradient(circle at 6.7% 75%, ${props => props.theme.blue}, rgba(0,0,255,0) 70.71%),
+      -webkit-linear-gradient(circle at 93.3% 75%, ${props => props.theme.yellow}, rgba(0,255,0,0) 100%) beige; /* Chrome10-25,Safari5.1-6 */
     background:
-      radial-gradient(circle at 50% 0, ${theme.red}, rgba(255,0,0,0) 50.71%),
-      radial-gradient(circle at 6.7% 75%, ${theme.blue}, rgba(0,0,255,0) 70.71%),
-      radial-gradient(circle at 93.3% 75%, ${theme.yellow}, rgba(0,255,0,0) 100%) beige; /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='${theme.red}', endColorstr='${theme.yellow}',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+      radial-gradient(circle at 50% 0, ${props => props.theme.red}, rgba(255,0,0,0) 50.71%),
+      radial-gradient(circle at 6.7% 75%, ${props => props.theme.blue}, rgba(0,0,255,0) 70.71%),
+      radial-gradient(circle at 93.3% 75%, ${props => props.theme.yellow}, rgba(0,255,0,0) 100%) beige; /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='${props => props.theme.red}', endColorstr='${props => props.theme.yellow}',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
   }
 `;
 
