c440df614215b3cc854eec78ce7096cfb6143f82
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2019-01-14 01:19:27 -0800

    UpdateCartItem component updates 'me' in cache after click

--- client/components/Buttons/UpdateCartItem.js
+++ client/components/Buttons/UpdateCartItem.js
@@ -1,12 +1,22 @@
 import PropTypes from 'prop-types';
 import { Mutation } from 'react-apollo';
-import { UPDATE_CARTITEM_MUTATION } from '../../graphql';
+import { UPDATE_CARTITEM_MUTATION, CURRENT_USER_QUERY } from '../../graphql';
 
 
 const UpdateCartItem = ({ id, quantity, disabled, children }) => {
+  const update = (cache, payload) => {
+    const data = cache.readQuery({ query: CURRENT_USER_QUERY });
+    const cartItemVariantId = payload.data.updateCartItem.variant.id;
+    data.me.cart = data.me.cart.map(cartItem => {
+      if (cartItem.variant.id === cartItemVariantId) cartItem.quantity = quantity;
+      return cartItem;
+    });
+    cache.writeQuery({ query: CURRENT_USER_QUERY, data });
+  };
   return (
     <Mutation mutation={UPDATE_CARTITEM_MUTATION}
       variables={{ id, quantity }}
+      update={update}
     >
       {(updateCartItem, { loading, error }) => (
         <button
