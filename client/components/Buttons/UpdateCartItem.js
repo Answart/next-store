3ca36b105f9a643e4e47cfc518ab8c6be11d5b3d
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2019-01-14 01:18:19 -0800

    UpdateCartItem component calls UPDATE_CARTITEM_MUTATION on click

--- client/components/Buttons/UpdateCartItem.js
+++ client/components/Buttons/UpdateCartItem.js
@@ -1,14 +1,26 @@
 import PropTypes from 'prop-types';
+import { Mutation } from 'react-apollo';
+import { UPDATE_CARTITEM_MUTATION } from '../../graphql';
 
 
 const UpdateCartItem = ({ id, quantity, disabled, children }) => {
   return (
+    <Mutation mutation={UPDATE_CARTITEM_MUTATION}
+      variables={{ id, quantity }}
+    >
+      {(updateCartItem, { loading, error }) => (
         <button
-          disabled={disabled}
-          onClick={(e) => console.log('updating', id, quantity)}
+          disabled={disabled || loading}
+          onClick={(e) => {
+            e.preventDefault();
+            updateCartItem()
+              .catch(err => alert(err.message.replace('GraphQL error: ', '')));
+          }}
         >
           {children}
         </button>
+      )}
+    </Mutation>
   );
 };
 
