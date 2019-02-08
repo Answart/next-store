4c7fa85f75092bf2971003ff18b3e5c7974dbb75
Alexandra Swart <answart@sbcglobal.net>
2019-02-08 01:45:31 -0800

    Switch mail operator for NODE_ENV parsing consistency

--- server/src/mail.js
+++ server/src/mail.js
@@ -1,23 +1,23 @@
 const nodemailer = require('nodemailer');
 
 
-const options = process.env.NODE_ENV === 'development'
-? {
-    host: process.env.MAILTRAP_HOST,
-    port: process.env.MAILTRAP_PORT,
-    auth: {
-      user: process.env.MAILTRAP_USER,
-      pass: process.env.MAILTRAP_PASS
+const options = (process.env.NODE_ENV === 'production')
+  ? {
+      host: process.env.POSTMARK_HOST,
+      port: process.env.POSTMARK_PORT,
+      auth: {
+        user: process.env.POSTMARK_USER,
+        pass: process.env.POSTMARK_PASS
+      }
     }
-  }
-: {
-    host: process.env.POSTMARK_HOST,
-    port: process.env.POSTMARK_PORT,
-    auth: {
-      user: process.env.POSTMARK_USER,
-      pass: process.env.POSTMARK_PASS
+  : {
+      host: process.env.MAILTRAP_HOST,
+      port: process.env.MAILTRAP_PORT,
+      auth: {
+        user: process.env.MAILTRAP_USER,
+        pass: process.env.MAILTRAP_PASS
+      }
     }
-  }
 
 const transport = nodemailer.createTransport({ ...options });
 
