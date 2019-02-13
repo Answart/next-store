0a2e3e8c00ccd5a86ade737e714748549e3308f3
Alexandra Swart <answart@sbcglobal.net>
2019-02-12 22:14:09 -0800

    Update lib/utils formatMoney fn to account for whole dollar amts

--- client/lib/utils.js
+++ client/lib/utils.js
@@ -83,6 +83,8 @@ const formatMoney = function(amount) {
     currency: 'USD',
     minimumFractionDigits: 2,
   };
+  // if its a whole, dollar amount, leave off the .00
+  if (amount % 1 === 0) options.minimumFractionDigits = 0;
   const formatter = new Intl.NumberFormat('en-US', options);
 
   return formatter.format(amount);
