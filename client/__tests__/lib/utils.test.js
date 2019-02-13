b782ab1b31856de9b40d3ee8e2d332ecd455bf09
Alexandra Swart <answart@sbcglobal.net>
2019-02-12 20:40:56 -0800

    Create lib __test__ dir with new utils.test file

---
+++ client/__tests__/lib/utils.test.js
@@ -0,0 +1,22 @@
+import {
+  objctsDiffer,
+  getUniqKeyVals,
+  getFltrdObjs,
+  capWord,
+  formatMoney,
+  getPageTitleProps,
+  getFilterProps,
+  getCartTotals
+} from '../../lib/utils';
+import product, {
+  variant,
+  variants,
+  products,
+  user,
+  user_two,
+  cart
+} from '../../lib/dummyData';
+
+
+describe('Util functions', () => {
+});
