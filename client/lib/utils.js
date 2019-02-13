5ebbad6d73dc10d663502d45b327808b446ca68e
Alexandra Swart <answart@sbcglobal.net>
2019-02-13 00:40:18 -0800

    Update lib/utils with commented out objctsDiffer fn as its currently unused

--- client/lib/utils.js
+++ client/lib/utils.js
@@ -1,74 +1,9 @@
 import { SALES_TAX_RATE, SHIPPING_COST_PER_ITEM } from '../config';
 
 
-const objctsDiffer = function(object1, object2) {
-  for (let propName in object1) {
-    if (object1.hasOwnProperty(propName) != object2.hasOwnProperty(propName)) {
-      return true;
-    } else if (typeof object1[propName] != typeof object2[propName]) {
-      return true;
-    }
-    if (!object2.hasOwnProperty(propName)) return true;
-  }
-
-  for (let propName in object2) {
-    const object1Val = object1[propName];
-    const object2Val = object2[propName];
-    const object1Type = typeof object1Val;
-    const object2Type = typeof object2Val;
-
-    if (object1.hasOwnProperty(propName) != object2.hasOwnProperty(propName)) {
-      return true;
-    } else if (typeof object1Val != typeof object2Val) {
-      return true;
-    }
-    if (!object1.hasOwnProperty(propName)) return true;
-
-    if (object1Type === 'array' && object2Type === 'array') {
-      if (objctsDiffer(object1Val, object2Val)) {
-        return true;
-      }
-    } else if (object1Type === 'object' && object2Type === 'object') {
-      if (objctsDiffer(object1Val, object2Val)) {
-        return true;
-      } else if (object1Val != object2Val) {
-        return true;
-      }
-    } else if (object1Type === 'string' && object2Type === 'string') {
-      if (object1Val != object2Val) return true;
-    }
-  }
-
-  return false;
-}
-
-const getUniqKeyVals = function(objs, key) {
-  let vals = [];
-
-  objs.map(obj => {
-    const val = obj[key];
-    if (!vals.includes(val)) vals.push(val);
-  });
-
-  return vals;
-}
-
-const getFltrdObjs = function(objs, filter) {
-  const filterKeys = Object.keys(filter);
-
-  return objs.filter(obj => {
-    for (let i = 0; i < filterKeys.length; i++) {
-      const key = filterKeys[i];
-      if (!obj[key]) return false;
-      if (obj[key] !== filter[key]) return false;
-    }
-    return true;
-  });
-}
-
 const capWord = function(string = "") {
   let new_string = (!string)
-    ? ''
+    ? ""
     : string.toString().trim();
   if (!new_string.length) return new_string;
 
@@ -176,14 +111,78 @@ const getCartTotals = function(cart = []) {
   };
 }
 
+const getUniqKeyVals = function(objs, key) {
+  let vals = [];
+
+  objs.map(obj => {
+    const val = obj[key];
+    if (!vals.includes(val)) vals.push(val);
+  });
+
+  return vals;
+}
+
+const getFltrdObjs = function(objs, filter) {
+  const filterKeys = Object.keys(filter);
+
+  return objs.filter(obj => {
+    for (let i = 0; i < filterKeys.length; i++) {
+      const key = filterKeys[i];
+      if (!obj[key]) return false;
+      if (obj[key] !== filter[key]) return false;
+    }
+    return true;
+  });
+}
+
+// const objsDiffer = function(object1, object2) {
+//   for (let propName in object1) {
+//     if (object1.hasOwnProperty(propName) != object2.hasOwnProperty(propName)) {
+//       return true;
+//     } else if (typeof object1[propName] != typeof object2[propName]) {
+//       return true;
+//     }
+//     if (!object2.hasOwnProperty(propName)) return true;
+//   }
+//
+//   for (let propName in object2) {
+//     const object1Val = object1[propName];
+//     const object2Val = object2[propName];
+//     const object1Type = typeof object1Val;
+//     const object2Type = typeof object2Val;
+//
+//     if (object1.hasOwnProperty(propName) != object2.hasOwnProperty(propName)) {
+//       return true;
+//     } else if (typeof object1Val != typeof object2Val) {
+//       return true;
+//     }
+//     if (!object1.hasOwnProperty(propName)) return true;
+//
+//     if (object1Type === 'array' && object2Type === 'array') {
+//       if (objsDiffer(object1Val, object2Val)) {
+//         return true;
+//       }
+//     } else if (object1Type === 'object' && object2Type === 'object') {
+//       if (objsDiffer(object1Val, object2Val)) {
+//         return true;
+//       } else if (object1Val != object2Val) {
+//         return true;
+//       }
+//     } else if (object1Type === 'string' && object2Type === 'string') {
+//       if (object1Val != object2Val) return true;
+//     }
+//   }
+//
+//   return false;
+// }
+
 
 export {
-  objctsDiffer,
-  getUniqKeyVals,
-  getFltrdObjs,
   capWord,
   formatMoney,
   getPageTitleProps,
   getFilterProps,
-  getCartTotals
+  getCartTotals,
+  getUniqKeyVals,
+  getFltrdObjs,
 };
