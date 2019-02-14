2809fef7292520fed5d753f343c2f60df0d03a02
Alexandra Swart <answart@sbcglobal.net>
2019-02-14 01:39:25 -0800

    Create AddToCart test

---
+++ client/__tests__/components/Buttons/AddToCart.test.js
@@ -0,0 +1,96 @@
+import wait from 'waait';
+import toJSON from 'enzyme-to-json';
+import { mount } from 'enzyme';
+import { MockedProvider } from 'react-apollo/test-utils';
+import { ApolloConsumer } from 'react-apollo';
+import { fakeVariant } from '../../../lib/testUtils';
+import { userQueryMock, userQueryCartItemMock, addToCartMutationMock } from '../../../lib/testMocks';
+import { AddToCart } from '../../../components/Buttons';
+import { CURRENT_USER_QUERY } from '../../../graphql';
+
+
+const mocks = [
+  { ...userQueryMock },
+  { ...userQueryCartItemMock },
+  { ...addToCartMutationMock }
+];
+
+describe('<AddToCart/>', () => {
+  it('renders and matches the snap shot', async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <AddToCart variant={fakeVariant()} disabled={false} />
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    expect(toJSON(wrapper.find('button'))).toMatchSnapshot();
+  });
+
+  it('adds an item to cart when clicked', async () => {
+    let apolloClient;
+    const variant = fakeVariant();
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <ApolloConsumer>
+          {client => {
+            apolloClient = client;
+            return <AddToCart variant={variant} disabled={false} />;
+          }}
+        </ApolloConsumer>
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    const { data: { me } } = await apolloClient.query({ query: CURRENT_USER_QUERY });
+    expect(me.cart).toHaveLength(0);
+    // add an item to the cart
+    wrapper.find('button').simulate('click');
+    await wait();
+    // check if the item is in the cart
+    const { data: { me: me2 } } = await apolloClient.query({ query: CURRENT_USER_QUERY });
+    expect(me2.cart).toHaveLength(1);
+    expect(me2.cart[0].id).toBe('omg123');
+    expect(me2.cart[0].quantity).toBe(3);
+  });
+
+  it('changes from add to adding when clicked', async () => {
+    const variant = fakeVariant();
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <AddToCart variant={variant} disabled={false} />
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    expect(wrapper.text()).toContain('Add To Cart');
+    wrapper.find('button').simulate('click');
+    expect(wrapper.text()).toContain('Adding To Cart');
+  });
+
+  it('does not add an item to cart when disabled', async () => {
+    const variant = fakeVariant();
+    let apolloClient;
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <ApolloConsumer>
+          {client => {
+            apolloClient = client;
+            return <AddToCart variant={variant} disabled={true} />;
+          }}
+        </ApolloConsumer>
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    const { data: { me } } = await apolloClient.query({ query: CURRENT_USER_QUERY });
+    expect(me.cart).toHaveLength(0);
+    // add an item to the cart
+    wrapper.find('button').simulate('click');
+    await wait();
+    expect(wrapper.text()).toContain('Add To Cart');
+    // check if the item is in the cart
+    const { data: { me: me2 } } = await apolloClient.query({ query: CURRENT_USER_QUERY });
+    expect(me2.cart).toHaveLength(0);
+  });
+});
