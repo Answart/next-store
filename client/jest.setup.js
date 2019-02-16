60509574ee0a119706ca21b52198612d22964568
Alexandra Swart <answart@sbcglobal.net>
2019-02-15 18:25:29 -0800

    Add jest globals alert/scrollTo/matchMedia

--- client/jest.setup.js
+++ client/jest.setup.js
@@ -2,3 +2,9 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+window.alert = (msg) => {
+  if (msg) console.log(msg);
+};
+window.matchMedia = () => ({});
+window.scrollTo = () => { };
