9488e8ffb8463275d789c4f0cfbb14ecd86e1856
Alexandra Swart <answart@sbcglobal.net>
2019-02-19 07:11:25 -0800

    Add UpdateProductForm.test file to __test__/components/Forms

--- client/__tests__/components/Forms/UpdateProductForm.test.js
+++ client/__tests__/components/Forms/UpdateProductForm.test.js
@@ -3,20 +3,20 @@ import wait from 'waait';
 import toJSON from 'enzyme-to-json';
 import Router from 'next/router';
 import { MockedProvider } from 'react-apollo/test-utils';
-import { CreateProductForm } from '../../../components/Forms';
+import { UpdateProductForm } from '../../../components/Forms';
 import {
   mockImage, mockProduct, mockImageVariables,
   createImageMutationMock,
-  createProductMutationMock, createProductMutationErrorMock,
+  updateProductMutationMock, updateProductMutationErrorMock,
 } from '../../../lib/test-utils/mocks';
 
 const successMocks = [
-  createImageMutationMock(),
-  { ...createProductMutationMock },
+  { ...createImageMutationMock({ productId: mockProduct.id }) },
+  { ...updateProductMutationMock },
 ];
 const errorMocks = [
-  createImageMutationMock(),
-  { ...createProductMutationErrorMock },
+  { ...createImageMutationMock({ productId: mockProduct.id }) },
+  { ...updateProductMutationErrorMock },
 ];
 // mock the global fetch API (cloudinary)
 global.fetch = jest.fn().mockResolvedValue({
@@ -34,13 +34,36 @@ global.fetch = jest.fn().mockResolvedValue({
   }),
 });
 
-
-describe('<CreateProductForm />', () => {
+describe('<UpdateProductForm />', () => {
   let wrapper;
   beforeAll(() => {
     wrapper = mount(
       <MockedProvider mocks={successMocks} addTypename={false}>
-        <CreateProductForm />
+        <UpdateProductForm
+          product={{
+            id: mockProduct.id,
+            department: 'bottoms',
+            title: 'some top',
+            description: 'some description',
+            category: 'pants',
+            brand: 'some brand',
+            online: true,
+            user: {
+              id: '123441232132',
+              name: 'hannah smith'
+            },
+            image: {
+              id: '777777',
+              cloudinary_id: 'clouuuuudid',
+              name: 'imageimage',
+              width: 22,
+              height: 44,
+              transformation: '',
+              image_url: 'some-image',
+              large_image_url: 'some-large-image',
+            }
+          }}
+        />
       </MockedProvider>
     );
   });
@@ -75,57 +98,54 @@ describe('<CreateProductForm />', () => {
       value: mockProduct.online, name: 'offline', type: 'radio'
     }});
     wrapper.update();
-    expect(wrapper.find('CreateProductForm').instance().state).toMatchObject({
+    expect(wrapper.find('UpdateProductForm').instance().state).toMatchObject({
+      id: mockProduct.id,
       title: mockProduct.title,
       department: mockProduct.department,
       category: mockProduct.category,
       description: mockProduct.description,
       brand: mockProduct.brand,
       online: mockProduct.online,
+      user: {
+        id: '123441232132',
+        name: 'hannah smith'
+      },
+      image: {
+        id: '777777',
+        cloudinary_id: 'clouuuuudid',
+        name: 'imageimage',
+        width: 22,
+        height: 44,
+        transformation: '',
+        image_url: 'some-image',
+        large_image_url: 'some-large-image',
+      },
+      message: ''
     });
   });
 
   it('image upload updates image in state', async () => {
-    wrapper.find('input[type="file"]').simulate('change', { target: {
-      files: [mockImage.image_url], name: 'image', type: 'file'
-    }});
+    wrapper.find('input[type="file"]')
+      .simulate('change', { target: {
+        files: [mockImage.image_url], name: 'image', type: 'file'
+      }});
     await wait(50);
     wrapper.update();
-    expect(wrapper.find('CreateProductForm').instance().state.image).toMatchObject({
+    expect(wrapper.find('UpdateProductForm').instance().state.image).toMatchObject({
       ...mockImageVariables,
       delete_token: 'delete tokeeeen'
     });
   });
 
-  it('routes to product/selections after successful product creation', async () => {
-    Router.router = { push: jest.fn() };
+  it('gives success message after product updated', async () => {
+    expect(wrapper.find('button').text()).toContain('Update Product');
     wrapper.find('form').simulate('submit');
+    expect(wrapper.find('button').text()).toContain('Updating Product');
     await wait(80);
-    expect(Router.router.push).toHaveBeenCalled();
-    expect(Router.router.push).toHaveBeenCalledWith({
-      pathname: '/product/selections', query: { id: mockProduct.id }
-    });
+    wrapper.update();
+    const state = wrapper.find('UpdateProductForm').instance().state;
+    const displayMess = wrapper.find('DisplayMessage');
+    expect(displayMess.text()).toBe('Success!Product changes saved.');
+    expect(state.message).toBe('Product changes saved.');
   });
-
-  // it('renders DisplayMessage component w/error message when submitted with invalid/insufficient info', async () => {
-  //   console.error = jest.fn();
-  //   const wrapper2 = mount(
-  //     <MockedProvider addTypename={false} mocks={errorMocks}>
-  //       <CreateProductForm />
-  //     </MockedProvider>
-  //   );
-  //   wrapper2.find('input[type="file"]')
-  //     .simulate('change', { target: {
-  //       files: [mockImage.image_url], name: 'image', type: 'file'
-  //     }});
-  //   await wait(50);
-  //   expect(global.fetch).toHaveBeenCalled();
-  //   Router.router = { push: jest.fn() };
-  //   wrapper2.find('form').simulate('submit');
-  //   await wait(50);
-  //   wrapper2.update();
-  //   expect(wrapper2.find('DisplayMessage').text()).toContain('Hold up! ack!');
-  //   expect(Router.router.push).not.toHaveBeenCalled();
-  //   wrapper2.unmount();
-  // });
 });
