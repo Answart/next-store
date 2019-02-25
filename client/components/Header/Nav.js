217549c6fc9ffb4403151fee038d142ff5fd9a57
Alexandra Swart <answart@sbcglobal.net>
2019-02-25 03:59:17 -0800

    Remove id from Links in Nav component

--- client/components/Header/Nav.js
+++ client/components/Header/Nav.js
@@ -6,11 +6,13 @@ import { capWord } from '../../lib/utils';
 const Nav = () => (
   <div className="hdr-nav">
     {DEPARTMENTS.map(dept => (
-      <Link key={dept} id={`nav-${dept}`} href={{
+      <Link key={dept} href={{
         pathname: "/shop",
         query: { department: dept.toLowerCase() }
       }}>
-        <a>{capWord(dept)}</a>
+        <a>
+          {capWord(dept)}
+        </a>
       </Link>
     ))}
   </div>
