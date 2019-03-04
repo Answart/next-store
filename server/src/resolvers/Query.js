dd588a3699ec33ea7f87586e28d596cd8cbc77af
Alexandra Swart <answart@sbcglobal.net>
2019-03-03 16:01:00 -0800

    Add cookie/isomorphic-unfetch/subscriptions-transport-ws to client packages

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -5,18 +5,17 @@ const { hasPermission } = require('../utils');
 const Query = {
   me(parent, args, ctx, info) {
     const id = ctx.request.userId;
-    console.log('me.id', id)
     if (!id) return null;
 
     return ctx.db.query.user({
       where: { id },
     }, info);
   },
-  async users(parent, args, ctx, info) {
+  users(parent, args, ctx, info) {
     if (!ctx.request.userId) throw new Error('USERS: You must be logged in!');
 
     // requester has permission to query all users?
-    hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+    hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE'])
 
     return ctx.db.query.users({}, info);
   },
