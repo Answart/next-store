0a1916deeb7a01630af191a335599a2167d362c9
Alexandra Swart <answart@sbcglobal.net>
2019-03-03 16:05:04 -0800

    Update RequireSignin component to render User component instead of querying CURRENT_USER_QUERY

--- client/components/RequireSignin.js
+++ client/components/RequireSignin.js
@@ -1,23 +1,28 @@
 import { Query } from 'react-apollo';
 import { SigninForm } from './Forms';
-import { CURRENT_USER_QUERY } from '../graphql';
+import NotFound from './NotFound';
+import User from './User';
 
 
-const RequireSignin = props => (
-  <Query query={CURRENT_USER_QUERY}>
-    {({ data, loading }) => {
-      if (loading) return <p>Loading...</p>;
+const RequireSignin = (props) => (
+  <User>
+    {({ loading, error, data }) => {
+      if (loading) return (<p>Loading...</p>);
+      if (error) return (<NotFound status={401} message={error.message} />);
       if (!data || !data.me) {
         return (
           <div>
-            <p>Please Sign In before Continuing</p>
+            <p>
+              Please Sign In before Continuing
+            </p>
+
             <SigninForm />
           </div>
         );
       }
       return props.children;
     }}
-  </Query>
+  </User>
 );
 
 
