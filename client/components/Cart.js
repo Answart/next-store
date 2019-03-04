00bac78c7b8e76fdad7439b46ccbf6fe25c5b335
Alexandra Swart <answart@sbcglobal.net>
2019-03-04 05:06:04 -0800

    Refactor Cart component to only query LOCAL_CARTOPEN_QUERY

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,6 +1,6 @@
 import { Query } from 'react-apollo';
 import { StyledCart } from './styles/CartStyles';
-import { StyledCartTable, StyledTotalsTable } from './styles/TableStyles';
+import { StyledCartTable, StyledTotalsTable, StyledCartItem } from './styles/TableStyles';
 import User from './User';
 import CartItem from './CartItem';
 import NotFound from './NotFound';
@@ -9,111 +9,108 @@ import { formatMoney, getCartTotals } from '../lib/utils';
 import { LOCAL_CARTOPEN_QUERY } from '../graphql';
 
 
-const Cart = () => (
+const Cart = ({ me }) => (
   <Query query={LOCAL_CARTOPEN_QUERY}>
-    {({ data: localData, error: localError }) => (
-      <User>
-        {({ data: userData, error: userError }) => {
-          if (localError) return null;
-          const me = !!userData ? userData.me : null;
-          const myCart = (!!me && !!me.cart) ? me.cart : [];
-          const imgUrl = (!!myCart.length && !!myCart[0].variant)
-            ? myCart[0].variant.image.image_url
-            : "";
-          const { totalQuantity, totalShipping, totalSalesTax, subTotal } = getCartTotals(myCart);
-          const totalAmount = (subTotal + totalShipping + totalSalesTax);
-          return (
-            <StyledCart open={localData.cartOpen}>
-              <header>
-                <div className="cart-title">
-                  <ToggleCart>
-                    <h2>&times;</h2>
-                  </ToggleCart>
+    {({ data: localData, error: localError }) => {
+      const myCart = (!!me && !!me.cart)
+        ? me.cart
+        : [];
+      const imgUrl = (!!myCart.length && !!myCart[0].variant)
+        ? myCart[0].variant.image.image_url
+        : "";
+      const { totalQuantity, totalShipping, totalSalesTax, subTotal } = getCartTotals(myCart);
+      const totalAmount = (subTotal + totalShipping + totalSalesTax);
+      return (
+        <StyledCart open={true}>
+          <header>
+            <div className="cart-title">
+              <ToggleCart>
+                <h2>&times;</h2>
+              </ToggleCart>
 
-                  <h2>Your Cart</h2>
-                </div>
-              </header>
+              <h2>Your Cart</h2>
+            </div>
+          </header>
 
-              <div className="cart-body">
-                <StyledCartTable>
-                  <thead>
-                    <tr>
-                      <th>Product</th>
+          <div className="cart-body">
+            <StyledCartTable>
+              <thead>
+                <tr>
+                  <th>Product</th>
 
-                      <th>Quantity</th>
+                  <th>Quantity</th>
 
-                      <th>Total Price</th>
-                    </tr>
-                  </thead>
+                  <th>Total Price</th>
+                </tr>
+              </thead>
 
-                  <tbody>
-                    {!!userError ? (
-                      <tr><td className="cart-page">
-                        <NotFound status={400} />
-                      </td></tr>
-                    ) : (
-                      <>
-                        {myCart.map(cartItem =>
-                          <CartItem key={cartItem.id}
-                            id={cartItem.id}
-                            quantity={cartItem.quantity}
-                            variant={cartItem.variant}
-                          />
-                        )}
-                      </>
+              <tbody>
+                {(true) ? (
+                  <StyledCartItem>
+                    <td className="cart-page">
+                      <NotFound status={400} />
+                    </td>
+                  </StyledCartItem>
+                ) : (
+                  <>
+                    {myCart.map(cartItem =>
+                      <CartItem key={cartItem.id} id={cartItem.id}
+                        quantity={cartItem.quantity}
+                        variant={cartItem.variant}
+                      />
                     )}
-                  </tbody>
-                </StyledCartTable>
-              </div>
+                  </>
+                )}
+              </tbody>
+            </StyledCartTable>
+          </div>
 
-              <footer>
-                <div>
-                  <StyledTotalsTable>
-                    <tbody>
-                      <tr>
-                        <td>Subtotal</td>
+          <footer>
+            <div>
+              <StyledTotalsTable>
+                <tbody>
+                  <tr>
+                    <td>Subtotal</td>
 
-                        <td>{formatMoney(subTotal)}</td>
-                      </tr>
+                    <td>{formatMoney(subTotal)}</td>
+                  </tr>
 
-                      <tr>
-                        <td>Shipping</td>
+                  <tr>
+                    <td>Shipping</td>
 
-                        <td>{formatMoney(totalShipping)}</td>
-                      </tr>
+                    <td>{formatMoney(totalShipping)}</td>
+                  </tr>
 
-                      <tr className="totals-table-last-item">
-                        <td>Sales Tax</td>
+                  <tr className="totals-table-last-item">
+                    <td>Sales Tax</td>
 
-                        <td>{formatMoney(totalSalesTax)}</td>
-                      </tr>
+                    <td>{formatMoney(totalSalesTax)}</td>
+                  </tr>
 
-                      <tr>
-                        <td>Estimated Total</td>
+                  <tr>
+                    <td>Estimated Total</td>
 
-                        <td>{formatMoney(totalAmount)}</td>
-                      </tr>
-                    </tbody>
-                  </StyledTotalsTable>
-                </div>
+                    <td>{formatMoney(totalAmount)}</td>
+                  </tr>
+                </tbody>
+              </StyledTotalsTable>
+            </div>
 
-                <div className="cart-close">
-                  <CheckoutCart
-                    totalQuantity={totalQuantity}
-                    totalAmount={totalAmount}
-                    email={!!me ? me.email : ""}
-                    image_url={imgUrl}
-                    disabled={!!userError || !me || totalQuantity === 0}
-                  >
-                    Checkout
-                  </CheckoutCart>
-                </div>
-              </footer>
-            </StyledCart>
-          )
-        }}
-      </User>
-    )}
+            <div className="cart-close">
+              <CheckoutCart
+                totalQuantity={totalQuantity}
+                totalAmount={totalAmount}
+                email={!!me ? me.email : ""}
+                image_url={imgUrl}
+                disabled={!me || totalQuantity === 0}
+              >
+                Checkout
+              </CheckoutCart>
+            </div>
+          </footer>
+        </StyledCart>
+      )
+    }}
   </Query>
 );
 
