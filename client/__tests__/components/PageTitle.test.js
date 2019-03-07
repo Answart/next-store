f1293da1f7e5cd10d05f9326f1b9db7814735015
Alexandra Swart <answart@sbcglobal.net>
2019-03-07 08:03:19 -0800

    Add PageTitle.test to __tests__/components/

---
+++ client/__tests__/components/PageTitle.test.js
@@ -0,0 +1,49 @@
+import { mount } from 'enzyme';
+import toJSON from 'enzyme-to-json';
+import wait from 'waait';
+import { MockedProvider } from 'react-apollo/test-utils';
+import PageTitle from '../../components/PageTitle';
+
+
+describe('<PageTitle />', () => {
+  let wrapper;
+  beforeAll(() => {
+    wrapper = mount(
+      <MockedProvider mocks={[]}>
+        <PageTitle
+          page='Edit Product'
+          titles={[{
+              label: 'Some Title',
+              href: {
+                pathname: '/product/edit',
+                query: {
+                  name: 'answart',
+                }
+              }
+            }, {
+              label: 'Selections',
+              href: {
+                pathname: '/product/selections',
+                query: {
+                  name: 'answart',
+                }
+              }
+            }, {
+              label: 'Add Selection'
+          }]}
+        />
+      </MockedProvider>
+    );
+  });
+  afterAll(() => wrapper.unmount());
+
+  it('matches the snap shot', async () => {
+    await wait();
+    wrapper.update();
+    expect(toJSON(wrapper.find('PageTitle'))).toMatchSnapshot();
+  });
+
+  it('renders properly', async () => {
+    expect(wrapper.text()).toContain('Edit Product≫Some Title≫Selections≫Add Selection');
+  });
+});
