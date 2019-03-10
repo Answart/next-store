8dcdc792e2755f955d9d6c2d4a263de2396ab666
Alexandra Swart <answart@sbcglobal.net>
2019-03-09 18:49:39 -0800

    Create basic sales page

---
+++ client/pages/sales.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import PageTitle from '../components/PageTitle';
+import { StyledOrderPage } from '../components/styles/PageStyles';
+
+
+const SalesPage = () => (
+  <StyledOrderPage>
+    <PageTitle page='Sales' />
+
+    <div className='order-page-content'>
+      TODO
+    </div>
+  </StyledOrderPage>
+);
+
+
+export default SalesPage;
