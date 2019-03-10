35dd4cb3f55764699cd71a69c71b7ce9e237bf85
Alexandra Swart <answart@sbcglobal.net>
2019-03-09 17:48:54 -0800

    Create basic orders page

---
+++ client/pages/orders.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import PageTitle from '../components/PageTitle';
+import { StyledOrderPage } from '../components/styles/PageStyles';
+
+
+const OrdersPage = () => (
+  <StyledOrderPage>
+    <PageTitle page='Order History' />
+
+    <div className='order-page-content'>
+      TODO
+    </div>
+  </StyledOrderPage>
+);
+
+
+export default OrdersPage;
