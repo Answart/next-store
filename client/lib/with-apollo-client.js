284396f74d0eefc194f121d47d0d7f5356036739
Alexandra Swart <answart@sbcglobal.net>
2019-03-12 12:19:02 -0700

    Refactor with-apollo-client error logging

--- client/lib/with-apollo-client.js
+++ client/lib/with-apollo-client.js
@@ -14,7 +14,6 @@ function parseCookies(req, options = {}) {
     ? cookie.parse(req.headers.cookie || '')
     : '';
 }
-
 function extractState(apolloClient) {
   let data = null;
 
@@ -68,7 +67,7 @@ export default App => {
           // Prevent Apollo Client GraphQL errors from crashing SSR.
           // Handle them in components via the data.error prop:
           // http://dev.apollodata.com/react/api-queries.html#graphql-query-data-error
-          console.error('Error while running `getDataFromTree`', error)
+          console.error(`Error while running 'getDataFromTree': ${error}`)
         }
         // getDataFromTree does not call componentWillUnmount
         // head side effect therefore need to be cleared manually
