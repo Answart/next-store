141602acada76c7131a812633a82bf10e0856f0e
Alexandra Swart <answart@sbcglobal.net>
2019-03-13 17:20:32 -0700

    Create server util fn getCartTotals

--- server/src/utils.js
+++ server/src/utils.js
@@ -1,3 +1,6 @@
+const SALES_TAX_RATE = 0.0925;
+const SHIPPING_COST_PER_ITEM = 1.2;
+
 
 function hasPermission(user, permissionsNeeded) {
   const matchedPermissions = user.permissions.filter(permissionTheyHave =>
@@ -6,6 +9,35 @@ function hasPermission(user, permissionsNeeded) {
   if (!matchedPermissions.length) {
     throw new Error(`You do not have sufficient permissions: ${permissionsNeeded}. You Have: ${user.permissions}`);
   }
-}
+};
 
-exports.hasPermission = hasPermission;
+function getCartTotals(cart = []) {
+  let quantity = 0;
+  let shipping = 0;
+  let tax = 0;
+  let subtotal = 0;
+
+  if (!!cart && !!cart.length) {
+    quantity = cart.reduce((tally, cartItem) => {
+      subtotal += cartItem.quantity * cartItem.price;
+      shipping += cartItem.quantity * SHIPPING_COST_PER_ITEM;
+      tax += cartItem.quantity * cartItem.price * SALES_TAX_RATE;
+
+      return tally + cartItem.quantity;
+    }, 0);
+  }
+
+  return {
+    quantity,
+    shipping: Number(parseFloat(shipping).toFixed(2)),
+    tax: Number(parseFloat(tax).toFixed(2)),
+    subtotal: Number(parseFloat(subtotal).toFixed(2)),
+    sales_tax_rate: SALES_TAX_RATE,
+    shipping_rate: SHIPPING_COST_PER_ITEM,
+  };
+};
+
+module.exports = {
+  hasPermission,
+  getCartTotals,
+}
