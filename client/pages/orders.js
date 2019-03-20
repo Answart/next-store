f23cf2912672fc3313cece9dad0b3044692b723b
Alexandra Swart <answart@sbcglobal.net>
2019-03-20 13:36:51 -0700

    orders page calls ORDERS_QUERY on render

--- client/pages/orders.js
+++ client/pages/orders.js
@@ -1,18 +1,41 @@
 import PropTypes from 'prop-types';
+import { Query } from 'react-apollo';
 import PageTitle from '../components/PageTitle';
 import RequireSignin from '../components/RequireSignin';
+import NotFound from '../components/NotFound';
 import { StyledOrderPage } from '../components/styles/PageStyles';
+import { getQueryVariables } from '../lib/utils';
+import { ORDERS_QUERY } from '../graphql';
 
 
-const OrdersPage = props => (
+const OrdersPage = ({ query }) => (
   <StyledOrderPage>
     <PageTitle page='Orders' />
 
     <div className='order-page-content'>
       <RequireSignin>
-        {({ me }) => (
-          <p>TODO</p>
-        )}
+        {({ me }) => {
+          const variables = getQueryVariables(query);
+          return (
+            <Query query={ORDERS_QUERY} variables={{
+              ...variables,
+              buyerId: me.id
+            }}>
+              {({ data, error, loading }) => {
+                if (loading) return (<p>Loading...</p>);
+                if (error) return (<NotFound status={400} message={error.message} />);
+                const { orders } = data;
+                console.log('orders', orders)
+                if (typeof orders === 'undefined' || orders === null) return (<NotFound status={404} />);
+                const count = orders.length;
+                if (orders.length < 1) return (<NotFound status={204} message='You do not have any orders yet.' />);
+                return (
+                  <p>TODO</p>
+                );
+              }}
+            </Query>
+          )
+        }}
       </RequireSignin>
     </div>
   </StyledOrderPage>
