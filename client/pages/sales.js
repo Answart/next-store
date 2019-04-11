1d831353bed6a766aeb0288283824e923afdb1c8
Alexandra Swart <answart@sbcglobal.net>
2019-04-11 00:01:02 -0700

    sales page uses getQueryVariables util fn for query variables

--- client/pages/sales.js
+++ client/pages/sales.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import PageTitle from '../components/PageTitle';
 import RequireSignin from '../components/RequireSignin';
 import { StyledOrderPage } from '../components/styles/PageStyles';
+import { getQueryVariables } from '../lib/utils';
 
 
 const SalesPage = (props) => (
@@ -11,6 +12,8 @@ const SalesPage = (props) => (
     <div className='order-page-content'>
       <RequireSignin>
         {({ me }) => {
+          const variables = getQueryVariables(query);
+          variables.sellerId = me.id;
           return (
             <p>TODO</p>
           )
