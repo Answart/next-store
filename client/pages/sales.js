46e319e60459bffbc50f4d93844aeb74b70c3fd6
Alexandra Swart <answart@sbcglobal.net>
2019-04-12 01:28:32 -0700

    sales page calls ORDER_ITEMS_QUERY on render

--- client/pages/sales.js
+++ client/pages/sales.js
@@ -1,11 +1,14 @@
+import { Query } from 'react-apollo';
 import PropTypes from 'prop-types';
 import PageTitle from '../components/PageTitle';
 import RequireSignin from '../components/RequireSignin';
+import NotFound from '../components/NotFound';
 import { StyledOrderPage } from '../components/styles/PageStyles';
 import { getQueryVariables } from '../lib/utils';
+import { ORDER_ITEMS_QUERY } from '../graphql';
 
 
-const SalesPage = (props) => (
+const SalesPage = ({ query }) => (
   <StyledOrderPage>
     <PageTitle page='Sales' />
 
@@ -15,7 +18,19 @@ const SalesPage = (props) => (
           const variables = getQueryVariables(query);
           variables.sellerId = me.id;
           return (
-            <p>TODO</p>
+            <Query query={ORDER_ITEMS_QUERY} variables={variables}>
+              {({ data, error, loading }) => {
+                if (loading) return (<p>Loading...</p>);
+                if (error) return (<NotFound status={400} message={error.message} />);
+                const { orderItems } = data;
+                if (typeof orderItems === 'undefined' || orderItems === null) return (<NotFound status={404} />);
+                if (orderItems.length < 1) return (<NotFound status={204} message='You do not have any sales yet.' />);
+                const count = orderItems.length;
+                return (
+                  <p>TODO</p>
+                );
+              }}
+            </Query>
           )
         }}
       </RequireSignin>
