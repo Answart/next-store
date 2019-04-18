bae6429f2f60f3fbd343d28b5f85053195cbbf8d
Alexandra Swart <answart@sbcglobal.net>
2019-04-18 00:55:41 -0700

    Update CartCount component with react-transition-group transitions

--- client/components/CartCount.js
+++ client/components/CartCount.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import { StyledCartCountAnimation, StyledCartCountDot } from './styles/CartStyles';
 
 
 const CartCount = ({ count }) => (
   <StyledCartCountAnimation>
+    <TransitionGroup>
+      <CSSTransition
+        className="count"
+        classNames="count"
+        key={count}
+        timeout={{ enter: 400, exit: 400 }}
+        unmountOnExit
+      >
         <StyledCartCountDot>
           <div className={!count ? '' : 'active-count'}>
             {count}
           </div>
         </StyledCartCountDot>
+      </CSSTransition>
+    </TransitionGroup>
   </StyledCartCountAnimation>
 );
 
