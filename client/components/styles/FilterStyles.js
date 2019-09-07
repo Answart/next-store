10eddf528fceec31bda75d546955756036ad774c
Alexandra Swart <answart@sbcglobal.net>
2019-09-06 20:47:46 -0700

    Add StyledFilterRange to FilterStyles directory

--- client/components/styles/FilterStyles.js
+++ client/components/styles/FilterStyles.js
@@ -78,7 +78,124 @@ const StyledFilter = styled.div`
   }
 `;
 
+const StyledFilterRange = styled.div`
+  padding: 0.5rem 1.5em;
+  display: block;
+  .filter-range-preview {
+    float: left;
+    font-weight: bold;
+    text-align: left;
+    display: inline-block;
+    width: 100%;
+    span {
+      &:nth-child(1) {
+        text-align: left;
+      }
+      &:nth-child(2) {
+        float: right;
+        text-align: right;
+      }
+    }
+  }
+  .filter-range {
+    /* background-color:yellow; */
+    position: relative;
+    display: block;
+    height: 3em;
+    width: 15em;
+    input[type=range] {
+      text-align: center;
+      display: block;
+      box-sizing: border-box;
+      appearance: none;
+      width:100%;
+      margin: 0;
+      padding: 5px 1.75px;
+      overflow: hidden;
+      border: 0;
+      border-radius: 1px;
+      outline: none;
+      background: linear-gradient(grey, grey) no-repeat center;
+      background-size: 100% 2px;
+      pointer-events: none;
+      cursor: pointer;
+      position: absolute;
+      &:active,
+      &:focus {
+        outline: none;
+      }
+      &:nth-child(2) {
+        background: none;
+        &::-webkit-slider-thumb::before {
+          background-color: grey;
+        }
+        &::-moz-range-thumb::before {
+          background-color: grey;
+        }
+      }
+      /* support webkit (e.g. Chrome/Safari) */
+      &::-webkit-slider-thumb {
+        min-height: 1em;
+        height: 1.5em;
+        min-width: 1em;
+        width: 1.5em;
+        border-radius: 28px;
+        background-color: #fff;
+        position: relative;
+        cursor: pointer;
+        appearance: none;
+        pointer-events: all;
+        outline: 0;
+        box-shadow: 0 1px 4px 1.5px rgba(0, 0, 0, 0.35);
+        &::before {
+          content: ' ';
+          display: block;
+          position: absolute;
+        }
+        z-index: 10;
+      }
+      /* support for Firefox */
+      &::-moz-range-thumb {
+        min-height: 1em;
+        height: 1.5em;
+        min-width: 1em;
+        width: 1.5em;
+        border-radius: 28px;
+        background-color: #fff;
+        position: relative;
+        margin: 5px 0;
+        cursor: pointer;
+        appearance: none;
+        pointer-events: all;
+        box-shadow: 0 1px 4px 0.5px rgba(0, 0, 0, 0.35);
+        &::before {
+          content: ' ';
+          display: block;
+          position: absolute;
+          top: 5px;
+        }
+        z-index: 10;
+        -moz-appearance: none;
+        background: linear-gradient(to bottom, #fff 0%, #fff 100%);
+      }
+      &::-moz-range-track {
+        position: relative;
+        z-index: -1;
+        border: 0;
+      }
+      &::last-of-type::-moz-range-track {
+        -moz-appearance: none;
+        border: 0;
+      }
+      /* support  */
+      &::-ms-thumb  {
+      }
+    }
+  }
+`;
+
 
 export {
   StyledFilter,
+  StyledFilterRange
 };
