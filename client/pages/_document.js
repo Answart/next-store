ddbc402c1428789ad1725369d80e3776d6aae6cb
Alexandra Swart <answart@sbcglobal.net>
2019-09-13 23:20:20 -0700

    Replace html tag with next/document Html

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 
@@ -12,7 +12,7 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <html>
+      <Html>
         <Head>
           {this.props.styleTags}
         </Head>
@@ -21,7 +21,7 @@ export default class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
